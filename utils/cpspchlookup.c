2c1fe1138a4a52fbc70e8b141240073470963f94
john-cvs <john-cvs>
1999-06-08 00:52:43 +0000

    includes and declaration changes.

--- utils/cpspchlookup.c
+++ utils/cpspchlookup.c
@@ -1,19 +1,20 @@
-#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
-float cpspch(float);
+#include <math.h>
+#include "../H/ugens.h"
 
-main(argc,argv)
-int argc;
-char *argv[];
+int
+main(int argc, char *argv[])
 {
-
-  float pch;
-  float cps;
   int i;
+
   if(argc == 1) {
     printf("you must specify pitch in 8ve.pc form\n");
     exit(0);
   }
   for(i=1; i< argc; i++)
     printf("%f = %f\n",atof(argv[i]),cpspch(atof(argv[i])));
+
+  return 0;
 }
+
--- utils/pchcpslookup.c
+++ utils/pchcpslookup.c
@@ -1,16 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-float pchcps(float);
+#include "../H/ugens.h"
 
-main(argc,argv)
-int argc;
-char *argv[];
+int
+main(int argc, char *argv[])
 {
   int i;
+
   if(argc == 1) {
     printf("you must specify pitch in hz\n");
     exit(0);
   }
   for(i=1; i< argc; i++)
     printf("%f = %f\n",atof(argv[i]),pchcps(atof(argv[i])));
+
+  return 0;
 }
+
