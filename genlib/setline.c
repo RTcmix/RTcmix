f820c6372b9a6021618326e0193b42b0650b16f7
john-cvs <john-cvs>
1999-06-11 00:12:07 +0000

    Applied Lansky's setline fix from last fall

--- genlib/setline.c
+++ genlib/setline.c
@@ -2,34 +2,59 @@
  * arguments is n_args, result is stuffed into array array of length length
  */
 
+#define PAUL_FIX                /* PL sent this fall '98? */
+
 #include <stdio.h>
 
-setline(float *p, short n_args,int length,float *array)
+setline(float *p, short n_args, int length, float *array)
 {
-	double increm;
-	int i,j,k,points;
+   double increm;
+   int i, j, k, i2, points;
 
-	if((n_args % 2) != 0) {
-		fprintf(stderr,"Something wrong with phrase, check args\n");
-		closesf();
-	}
+   if ((n_args % 2) != 0) {
+      fprintf(stderr, "Something wrong with phrase, check args\n");
+      closesf();
+   }
 
-	increm = (double)(p[n_args - 2] - p[0])/(double)length;
-	for(j=0,i=0; j < (n_args-2); j += 2) {
-		points = (int)((double)(p[j+2] - p[j]) / increm +.5);
-		if(p[j+2] != p[j]) {
-			if(points <= 0) points = 1;
-			if((p[j+2] < p[j]) || (points > length)) {
-				fprintf(stderr," confusion on phrase card\n");
-				closesf();
-				}
-			for(k=0; k < points; k++) {
-				array[i++] = ((float)k/(float)points)
-					* (p[j+3] - p[j+1]) + p[j+1];
-				if(i == length) return;
-			}
-		}
-	}
-	i--;
-	while(++i < length) array[i] = array[i-1]; 
+   /* increm is time span for each array slot */
+   increm = (double) (p[n_args - 2] - p[0]) / (double) length;
+
+   for (j = 0, i = 0; j < (n_args - 2); j += 2) {
+      points = (int) ((double) (p[j + 2] - p[j]) / increm + .5);
+      if (p[j + 2] != p[j]) {
+         if (points <= 0)
+            points = 1;
+         if ((p[j + 2] < p[j]) || (points > length)) {
+            fprintf(stderr, " confusion on phrase card\n");
+            closesf();
+         }
+#ifdef PAUL_FIX
+         i = (double) p[j] / increm + .5;      /* force right position */
+         i2 = (double) p[j + 2] / increm + .5;
+
+         for (k = i; k < i2; k++) {
+            array[k] = ((float) (k - i) / (float) points)
+                       * (p[j + 3] - p[j + 1]) + p[j + 1];
+
+            if (k == length)
+               return;
+         }
+      }
+   }
+   k--;
+   while (++k < length)
+      array[k] = array[k - 1];
+#else
+         for (k = 0; k < points; k++) {
+            array[i++] = ((float) k / (float) points)
+                * (p[j + 3] - p[j + 1]) + p[j + 1];
+            if (i == length)
+               return;
+         }
+      }
+   }
+   i--;
+   while (++i < length)
+      array[i] = array[i - 1];
+#endif
 }
