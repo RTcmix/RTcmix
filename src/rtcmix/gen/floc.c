31d1fb7b84b114ddc1078e8bd108453e5d9b4292
john-cvs <john-cvs>
1999-06-12 13:09:37 +0000

    Now complains about non-existant gen if genno != 1. If genno == 1 and doesn't exist, returns NULL without complaining.

--- src/rtcmix/gen/floc.c
+++ src/rtcmix/gen/floc.c
@@ -1,25 +1,38 @@
 #include "../H/ugens.h"
 #include <stdio.h>
 
-extern int ngens;       /*total of gens so far, initialized in main at 1*/
+/* these 3 defined in makegen.c */
 extern float *farrays[];
 extern int sizeof_farray[];
 extern int f_goto[];
 
+/* Returns the address of function number genno.
+   If genno is 1, then returns NULL if that function array doesn't exist.
+   This is part of the new setline scheme:
+      m_setline uses genno 1 in its call to makegen, and
+      insts assume that genno 1 contains the overall amplitude envelope.
+      If genno 1 doesn't exist, insts use 1.0 as an amp multiplier, rather
+        than grabbing one out of a function table. (No more lineset flag.)
+   If genno is not 1 and doesn't exist, complains and bails out.
+
+   NOTE: Some insts don't use genno 1 as an amp envelope, so they need to
+   check the return of floc and complain themselves if it's NULL.
+*/
 
 float *floc(int genno)
-/*       returns the address of function number genno  */
 {
-#ifdef NOMORE
-	if(!sizeof_farray[f_goto[genno]]) {
-               fprintf(stderr,"floc: You haven't allocated function %d yet.\n",genno);
-		closesf();
-	}
-	return(farrays[f_goto[genno]]);
-#else
-	if (sizeof_farray[f_goto[genno]] == 0)
-		return NULL;
-	else
-		return farrays[f_goto[genno]];
-#endif
+   int index = f_goto[genno];
+   int size = sizeof_farray[index];
+
+   if (size == 0) {
+      if (genno == 1)
+         return NULL;
+      else {
+         fprintf(stderr, "You haven't allocated function %d yet!\n", genno);
+         closesf();
+      }
+   }
+   else
+      return farrays[index];
 }
+
