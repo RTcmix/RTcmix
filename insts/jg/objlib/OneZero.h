ed3c08a2b1a1d21cb1d0c2b24bc39bd3923916f0
john-cvs <john-cvs>
1999-07-22 14:38:31 +0000

    reformatted comment (woohoo)

--- insts/jg/objlib/OneZero.h
+++ insts/jg/objlib/OneZero.h
@@ -1,17 +1,11 @@
-/*******************************************/
-/*  One Zero Filter Class,                 */
-/*  by Perry R. Cook, 1995-96              */ 
-/*  The parameter gain is an additional    */
-/*  gain parameter applied to the filter   */  
-/*  on top of the normalization that takes */
-/*  place automatically.  So the net max   */
-/*  gain through the system equals the     */
-/*  value of gain.  sgain is the combina-  */
-/*  tion of gain and the normalization     */
-/*  parameter, so if you set the poleCoeff */
-/*  to alpha, sgain is always set to       */
-/*  gain / (1.0 - fabs(alpha)).            */
-/*******************************************/
+/* One Zero Filter Class, by Perry R. Cook, 1995-96
+   The parameter <gain> is an additional gain parameter applied to the
+   filter on top of the normalization that takes place automatically.
+   So the net max gain through the system equals the value of <gain>.
+   <sgain> is the combination of <gain> and the normalization parameter,
+   so if you set the zeroCoeff to alpha, sgain is always set to
+   gain / (1.0 + fabs(alpha)).
+*/
 
 #if !defined(__OneZero_h)
 #define __OneZero_h
