178663b6ff187e1da08323028f67f1c4a91f6849
djt7p <djt7p>
1999-09-29 22:19:33 +0000

    Moved here.

---
+++ src/rtcmix/RTsockfuncs.h
@@ -0,0 +1,31 @@
+/* RTsockfuncs.h - headers for a set of utility functions for real-time cmix
+   interface programming (located in RTsockfuncs.c; link in RTsockfuncs.o
+   to compile) */
+
+#include "sockdefs.h"
+
+int newRTsock(char *ihost, int rtsno);
+
+int RTsock(char *ihost, int rtsno);
+
+int RTopensocket(int socket, char *binaryname);
+
+void RTkillsocket(int socket, int pid);
+
+void RTsendsock(const char *cmd, int theSock, int nargs, ...);
+
+void RTsetsockstr(struct sockdata *ssend, char *cmd, int nargs, double *fields);
+
+void RTsetsockdata(struct sockdata *ssend, int arg, double val);
+
+void RTprintsockstr(struct sockdata *ssend);
+
+void RTsendsockstr(int theSock, struct sockdata *sockstr);
+
+RTsockstr *RTnewsockstr(char *name);
+
+void RTtimeit(float interval, void *func);
+
+void parse(char *buf, char **args);
+
+int execute(char **args);
