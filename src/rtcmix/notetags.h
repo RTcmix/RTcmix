5f26c8b33f71ca7eff1cb47bd69615b635a3326f
gibson <gibson>
2000-02-11 01:44:46 +0000

    Fold this into globals.h

--- src/rtcmix/notetags.h
+++ src/rtcmix/notetags.h
@@ -1,9 +1,1 @@
-#define MAXPUPARR 100
-#define MAXPUPS 20
-#define NOPUPDATE 78787878 // hopefully never a real p-value!
-
-extern int curtag;
-extern int tags_on;
-extern int tag_sem;
-
-extern float pupdatevals[MAXPUPARR][MAXPUPS];
+#include <globals.h>
