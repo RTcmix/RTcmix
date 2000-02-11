6d9f2d3f6d2eec9d6330dcf2cd94dd1a7e900d0e
gibson <gibson>
2000-02-11 01:53:49 +0000

    Add copyright notice.

--- src/rtcmix/rtdispatch.cpp
+++ src/rtcmix/rtdispatch.cpp
@@ -1,3 +1,7 @@
+/* RTcmix  - Copyright (C) 2000  The RTcmix Development Team
+   See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
+   the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
+*/
 #include "../sys/mixerr.h"
 
 extern "C" double checkInsts(char*, double*, short);
