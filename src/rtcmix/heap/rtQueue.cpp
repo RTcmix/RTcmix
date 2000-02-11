68cdda6a582cd0be391936b2fc67b5dbf220d3e4
gibson <gibson>
2000-02-11 02:10:09 +0000

    Add copyright notice.

--- src/rtcmix/heap/rtQueue.cpp
+++ src/rtcmix/heap/rtQueue.cpp
@@ -1,3 +1,7 @@
+/* RTcmix  - Copyright (C) 2000  The RTcmix Development Team
+   See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
+   the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
+*/
 #include <iostream.h>
 #include "heap.h"
 #include "../../H/dbug.h"
