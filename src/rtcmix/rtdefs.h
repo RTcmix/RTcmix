e8a8ac08d9abe98ef3ecba4766e5f2665313ce3b
gibson <gibson>
2000-02-18 20:18:43 +0000

    Change "AUDIO_DEVICE" to "AUDIO_DEVICE_FDINDEX", and change its value. Add new "NO_DEVICE_FDINDEX".

--- src/rtcmix/rtdefs.h
+++ src/rtcmix/rtdefs.h
@@ -3,8 +3,10 @@
 #define _RTDEFS_H_ 1
 
 #define MAXCHANS 4
-#define MAX_INPUT_FDS 128
-#define AUDIO_DEVICE 9999999
+
+#define MAX_INPUT_FDS        128
+#define NO_DEVICE_FDINDEX    -1
+#define AUDIO_DEVICE_FDINDEX -2
 
 /* definition of input file desc struct used by rtinput */
 typedef struct inputdesc {
