1fb0abd9d598567cad43e7f0e523787ac8a9bbd4
gibson <gibson>
2000-02-18 02:16:20 +0000

    Added some new fields and functions to Instrument class (for new bus scheme). Removed virtual keyword for some accessor functions -- hope this is right. Minor cleanups.

--- src/rtcmix/Instrument.h
+++ src/rtcmix/Instrument.h
@@ -1,50 +1,75 @@
 /* RTcmix  - Copyright (C) 2000  The RTcmix Development Team
    See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
    the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
-*/
-#ifndef _INSTRUMENT_H_ 
+ */
+#ifndef _INSTRUMENT_H_
 #define _INSTRUMENT_H_ 1
+
+#include <bus.h>
+#include <buffers.h>
 #include <sys/types.h>
+
 
 class Instrument {
 public:
-	float start;
-	float dur;
-	int cursamp;
-	int chunksamps;
-	int endsamp;
-	int nsamps;
-	unsigned long chunkstart;
+   float          start;
+   float          dur;
+   int            cursamp;
+   int            chunksamps;
+   int            endsamp;
+   int            nsamps;
+   unsigned long  chunkstart;
+   int            output_offset;
 
-	int sfile_on; 		// a soundfile is open (for closing later)
-	int fdIndex;            // index into unix input file desc. table
-	int inputchans;
-	double inputsr;
-	off_t fileOffset;       // current offset in file for this inst
-	int mytag;              // for note tagging/rtupdate()	
+   int            sfile_on;        // a soundfile is open (for closing later)
+   int            fdIndex;         // index into unix input file desc. table
+   off_t          fileOffset;      // current offset in file for this inst
 
-	Instrument();
-	virtual ~Instrument();
-	virtual int init(float*, short);
-	virtual int run();
+   double         inputsr;
+   int            inputchans;
+   int            outputchans;
 
-	virtual float getstart();
-	virtual float getdur(); 
-	virtual int getendsamp();
-	virtual void setendsamp(int);
-	virtual void setchunk(int);
-	virtual void setchunkstart(int);
+   int            mytag;           // for note tagging/rtupdate() 
+
+protected:
+   BUFTYPE        *inbuf;          // private interleaved buffers
+   BUFTYPE        *outbuf;
+
+   BusSlot        *bus_config;
+
+public:
+   Instrument();
+   virtual ~Instrument();
+   virtual int init(float *, short);
+   virtual int run();
+
+   void exec();
+   void addout(BusType bus_type, int bus);
+
+   float getstart();
+   float getdur();
+   int getendsamp();
+   void setendsamp(int);
+   void setchunk(int);
+   void setchunkstart(int);
+   void setchunkstart(int);
+   void set_output_offset(int);
+
 private:
-	void gone(); // decrements reference to input soundfile
+   void gone();                    // decrements reference to input soundfile
+
 };
 
-extern void heapify(Instrument *Iptr);
-extern void heapSched(Instrument *Iptr);
-extern int rtsetoutput(float, float, Instrument*);
-extern int rtsetinput(float, Instrument*);
-extern int rtaddout(float*);
-extern int rtbaddout(float*, int);
-extern int rtgetin(float*, Instrument*, int);
-extern float rtupdate(int, int); // tag, p-field for return value
+
+// some prototypes that probably don't belong here
+void heapify(Instrument * Iptr);
+void heapSched(Instrument * Iptr);
+int rtsetoutput(float, float, Instrument *);
+int rtsetinput(float, Instrument *);
+int rtaddout(float *);
+int rtbaddout(float *, int);
+int rtgetin(float *, Instrument *, int);
+float rtupdate(int, int);  // tag, p-field for return value
+
 
 #endif /* _INSTRUMENT_H_  */
