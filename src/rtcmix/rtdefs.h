663979c5dc399361e1946cd9269834c2d7077d92
gibson <gibson>
2000-02-18 15:51:11 +0000

    Strip out non USE_SNDLIB code.

--- src/rtcmix/rtdefs.h
+++ src/rtcmix/rtdefs.h
@@ -8,21 +8,15 @@
 
 /* definition of input file desc struct used by rtinput */
 typedef struct inputdesc {
-	char filename[1024];
-	int fd;
-	int refcount;
-#ifdef USE_SNDLIB
-	short header_type;        /* e.g., AIFF_sound_file (in sndlib.h) */
-	short data_format;        /* e.g., snd_16_linear (in sndlib.h) */
-	short chans;
-	float srate;
-#else
-  #ifdef sgi
-	void *handle;
-  #endif
-#endif
-	int data_location;        /* offset of sound data start in file */
-	float dur;
+   char  filename[1024];
+   int   fd;
+   int   refcount;
+   short header_type;           /* e.g., AIFF_sound_file (in sndlib.h) */
+   short data_format;           /* e.g., snd_16_linear (in sndlib.h) */
+   short chans;
+   float srate;
+   int   data_location;           /* offset of sound data start in file */
+   float dur;
 } InputDesc;
 
 /* for insts - so they don't have to include globals.h */
@@ -32,4 +26,3 @@ extern int RTBUFSAMPS;
 extern float SR;
 
 #endif /* _RTDEFS_H_ */
-
