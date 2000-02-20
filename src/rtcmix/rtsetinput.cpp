b7b27e9d41b12de2a012402df68fd0eda1193d45
gibson <gibson>
2000-02-20 05:53:09 +0000

    Accommodate aux input.

--- src/rtcmix/rtsetinput.cpp
+++ src/rtcmix/rtsetinput.cpp
@@ -23,48 +23,52 @@ extern int get_last_input_index(void);       /* defined in rtinput.c */
 int
 rtsetinput(float start_time, Instrument *inst)
 {
-   int   index;
-
-   index = get_last_input_index();
-
-   if (index < 0 || inputFileTable[index].fd < 1) {
-      fprintf(stderr, "No input source open for this instrument!\n");
-      return -1;
+   if (inst->bus_config->auxin_count > 0) {
+// FIXME: not sure we have to do anything here  -JGG
    }
 
-   /* File or audio device was opened in rtinput(). Here we store the
-      index into the inputFileTable for the file or device.
-   */
-   inst->fdIndex = index;
+   if (inst->bus_config->in_count > 0) {
+      int index = get_last_input_index();
 
-   /* Fill in relevant data members of instrument class. */
-   inst->inputsr = inputFileTable[index].srate;
+      if (index < 0 || inputFileTable[index].fd < 1) {
+         fprintf(stderr, "No input source open for this instrument!\n");
+         return -1;
+      }
 
-// FIXME: not sure this is how we should do it. inputchans set according
-// to what bus_config specifies. Does user necessarily want that to change
-// to conform to sound file chans? Maybe just a warning and no change?  -JGG
-   if (inst->inputchans > inputFileTable[index].chans) {
-      fprintf(stderr, "WARNING: Changing bus config to match input chans.\n");
-      inst->inputchans = inputFileTable[index].chans;
+      /* File or audio device was opened in rtinput(). Here we store the
+         index into the inputFileTable for the file or device.
+      */
+      inst->fdIndex = index;
+
+      /* Fill in relevant data members of instrument class. */
+      inst->inputsr = inputFileTable[index].srate;
+
+// FIXME: not sure what to do here. inputchans is set according to what
+// bus_config specifies. Currently, we just warn about the descrepancy,
+// but maybe we should do something about it?  -JGG
+      if (inst->inputchans != inputFileTable[index].chans) {
+         fprintf(stderr, "WARNING: This instrument's bus config doesn't "
+                         "match the number of input channels.\n");
+      }
+
+      if (!inputFileTable[index].is_audio_dev) {
+         int datum_size = sizeof(short);
+         int inskip = (int) (start_time * inst->inputsr);
+
+         inst->sfile_on = 1;
+
+         /* Offset is measured from location that is set up in rtinput(). */
+         inst->fileOffset = inputFileTable[index].data_location
+                            + (inskip * inst->inputchans * datum_size);
+
+         if (start_time >= inputFileTable[index].dur)
+            fprintf(stderr, "\nWARNING: Attempt to read past end of input "
+                            "file: %s\n\n", inputFileTable[index].filename);
+      }
+
+      /* We also increment the reference count for this file. */
+      inputFileTable[index].refcount++;
    }
-
-   if (!inputFileTable[index].is_audio_dev) {
-      int datum_size = sizeof(short);
-      int inskip = (int) (start_time * inst->inputsr);
-
-      inst->sfile_on = 1;
-
-      /* Offset is measured from location that is set up in rtinput(). */
-      inst->fileOffset = inputFileTable[index].data_location
-                         + (inskip * inst->inputchans * datum_size);
-
-      if (start_time >= inputFileTable[index].dur)
-         fprintf(stderr, "\nWARNING: Attempt to read past end of input "
-                         "file: %s\n\n", inputFileTable[index].filename);
-   }
-
-   /* We also increment the reference count for this file. */
-   inputFileTable[index].refcount++;
 
    return 0;
 }
