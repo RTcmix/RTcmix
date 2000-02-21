cc1df7b38beb112bb317facb1240e81ad633c32a
gibson <gibson>
2000-02-21 03:57:30 +0000

    Add types for buffers that interface with sound driver. Add more function prototypes.

--- src/rtcmix/buffers.h
+++ src/rtcmix/buffers.h
@@ -10,21 +10,40 @@ extern "C" {
 #endif /* __cplusplus */
 
 
-#define BUFTYPE float             /* could be double some day */
-
+/* type of buffer used for internal buses */
+#define BUFTYPE float           /* could be double some day */
 typedef BUFTYPE *BufPtr;
 
-void init_buf_ptrs(void);
-int allocate_audioin_buffer(short chan, int frames);
-int allocate_aux_buffer(short chan, int frames);
-int allocate_out_buffer(short chan, int frames);
+/* type of buffer used to send to or receive from audio device */
+#ifdef LINUX
+#define OBUFTYPE short
+typedef OBUFTYPE *OBufPtr;
+#define IBUFTYPE short
+typedef IBUFTYPE *IBufPtr;
+#endif /* LINUX */
+#ifdef SGI
+#define OBUFTYPE short        /* could make this float, as in CMC version ... */
+//#define OBUF_IS_FLOAT       /* ... and also define this */
+typedef OBUFTYPE *OBufPtr;
+#define IBUFTYPE short
+typedef IBUFTYPE *IBufPtr;
+#endif /* SGI */
+
 void copy_interleaved_buf_to_one_buf(BufPtr dest, const BufPtr src,
                                 int src_chans, int src_chan, int dest_frames);
 void copy_one_buf_to_interleaved_buf(BufPtr dest, const BufPtr src,
                               int dest_chans, int dest_chan, int dest_frames);
 void copy_interleaved_buf_to_buf(BufPtr dest, const BufPtr src, int dest_chans,
                  int dest_chan, int dest_frames, int src_chans, int src_chan);
-
+void init_buf_ptrs(void);
+void clear_audioin_buffers(void);
+void clear_aux_buffers(void);
+void clear_output_buffers(void);
+int allocate_audioin_buffer(short chan, int nsamps);
+int allocate_aux_buffer(short chan, int nsamps);
+int allocate_out_buffer(short chan, int nsamps);
+IBufPtr allocate_ibuf_ptr(int nsamps);
+OBufPtr allocate_obuf_ptr(int nsamps);
 
 #ifdef __cplusplus
 } /* extern "C" */
