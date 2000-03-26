d5ab1eff5e7361c13864e24806a33f2c25a26de5
gibson <gibson>
2000-03-26 05:13:48 +0000

    Added dbamp function.

--- genlib/ampdb.c
+++ genlib/ampdb.c
@@ -4,3 +4,10 @@ double ampdb(float db)
 {
 	return(pow(10.,(double)(db/20.)));
 }
+
+double dbamp(float amp)    /* added by JG, 3/25/00 */
+{
+   double fabs_amp = fabs((double) amp);
+   return (20.0 * log10(fabs_amp));
+}
+
