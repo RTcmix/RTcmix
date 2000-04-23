4caff712ba6a92a75588617feb33f066180143f1
gibson <gibson>
2000-04-23 03:16:04 +0000

    Fixing previous mistaken commit.

--- insts/jg/denoise/cfr4tr.c
+++ insts/jg/denoise/cfr4tr.c
@@ -1,37 +1,36 @@
-#include <math.h>
+# include <math.h>
 
-/*-------------------------------
-	cfr4syn.c
+/*---------------------------
+	cfr4tr.c
 
-radix 4 synthesis
--------------------------------*/
+Radix 4 iteration subroutine.
+---------------------------*/
 
 void
-cfr4syn(long off, long nn, float *b0, float *b1, float *b2, float *b3,
-                           float *b4, float *b5, float *b6, float *b7)
+cfr4tr(long off, long nn, float *b0, float *b1, float *b2, float *b3,
+                          float *b4, float *b5, float *b6, float *b7)
 {
-	extern	double	pii, p7two;
-
-	float	piovn, arg, c1, c2, c3, s1, s2, s3, 
-		t0, t1, t2, t3, t4, t5, t6, t7;
+	extern	double	pii, p7;
 
 	long	i, off4, jj0, jlast, k0, kl, ji, jl, jr, j, k,
 		jj1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12,
 		j13, j14, j15, j16, j17, j18, jthet, th2,
 		l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12,
-		l13, l14, l15, l16, l17, l18, l19,
-		l[19];
+		l13, l14, l15, l16, l17, l18, l19,  l[19];
+
+	float	piovn, arg, c1, c2, c3, s1, s2, s3, r1, r5, pr, pi,
+		t0, t1, t2, t3, t4, t5, t6, t7;
 
 /* jthet is a reversed binary counter; jr steps two at a time to
 	locate real parts of intermediate results, and ji locates
 	the imaginary part corresponding to jr.
 */
 
-	l[0] = nn/4;
+	l[0] = nn / 4;
 	for (i=1; i<19; i++){
-		if (l[i-1]<2) l[i-1] = 2;
-		else if (l[i-1]!=2) {
-			l[i] = l[i-1]/2;
+		if (l[i-1] < 2) l[i-1] = 2;
+		else if (l[i-1] != 2){
+			l[i] = l[i-1] / 2;
 			continue;
 		}
 		l[i] = 2;
@@ -62,7 +61,7 @@ cfr4syn(long off, long nn, float *b0, float *b1, float *b2, float *b3,
 	jl = 2;
 	jr = 2;
 
-	/* for (jj1=2; jj1<=l1; jj1+=2){ */
+	/* for (j1=2; jj1<=l1; jj1+=2){ */
 	jj1 = 2;
 loop1:	 /* for (j2=jj1; j2<=l2; j2+=l1){ */
 	 j2 = jj1;
@@ -87,10 +86,32 @@ loop5:	     for (j6=j5; j6<=l6; j6+=l5){
 			 for (j18=j17; j18<=l18; j18+=l17){
 			  for (jthet=j18; jthet<=l19; jthet+=l18){
 			   th2 = jthet - 2;
-			   if (th2 > 0) {
+			   if (th2 <= 0){
+			    for (i=0; i<off; i++){
+			     t0 = b0[i] + b2[i];
+			     t1 = b1[i] + b3[i];
+			     b2[i] = b0[i] - b2[i];
+			     b3[i] = b1[i] - b3[i];
+			     b0[i] = t0 + t1;
+			     b1[i] = t0 - t1;
+			    }
+			    if (nn > 4){
+			     k0 = 4 * off;
+			     kl = k0 + off;
+			     for (i=k0; i<kl; i++){
+			      pr = p7 * (b1[i] - b3[i]);
+			      pi = p7 * (b1[i] + b3[i]);
+			      b3[i] = b2[i] + pi;
+			      b1[i] = pi - b2[i];
+			      b2[i] = b0[i] - pr;
+			      b0[i] = b0[i] + pr;
+			     }
+			    }
+			   }
+			   else{
 			    arg = th2 * piovn;
 			    c1 = cos(arg);
-			    s1 = -sin(arg);
+			    s1 = sin(arg);
 			    c2 = c1*c1 - s1*s1;
 			    s2 = c1*s1 + c1*s1;
 			    c3 = c1*c2 - s1*s2;
@@ -100,53 +121,35 @@ loop5:	     for (j6=j5; j6<=l6; j6+=l5){
 			    k0 = ji * off4;
 			    jlast = jj0 + off;
 			    for (j=jj0; j<jlast; j++){
-			     k = k0+j-jj0;
-			     t0 = b0[j] + b6[k];
-			     t1 = b7[k] - b1[j];
-			     t2 = b0[j] - b6[k];
-			     t3 = b7[k] + b1[j];
-			     t4 = b2[j] + b4[k];
-			     t5 = b5[k] - b3[j];
-			     t6 = b5[k] + b3[j];
-			     t7 = b4[k] - b2[j];
-			     b0[j] = t0 + t4;
-			     b4[k] = t1 + t5;
-			     b1[j] = (t2+t6)*c1 - (t3+t7)*s1;
-			     b5[k] = (t2+t6)*s1 + (t3+t7)*c1;
-			     b2[j] = (t0-t4)*c2 - (t1-t5)*s2;
-			     b6[k] = (t0-t4)*s2 + (t1-t5)*c2;
-			     b3[j] = (t2-t6)*c3 - (t3-t7)*s3;
-			     b7[k] = (t2-t6)*s3 + (t3-t7)*c3;
+			     k = k0 + j - jj0;
+			     r1 = b1[j]*c1 - b5[k]*s1;
+			     r5 = b1[j]*s1 + b5[k]*c1;
+			     t2 = b2[j]*c2 - b6[k]*s2;
+			     t6 = b2[j]*s2 + b6[k]*c2;
+			     t3 = b3[j]*c3 - b7[k]*s3;
+			     t7 = b3[j]*s3 + b7[k]*c3;
+			     t0 = b0[j]+t2;
+			     t4 = b4[k]+t6;
+			     t2 = b0[j]-t2;
+			     t6 = b4[k]-t6;
+			     t1 = r1+t3;
+			     t5 = r5+t7;
+			     t3 = r1-t3;
+			     t7 = r5-t7;
+			     b0[j] = t0+t1;
+			     b7[k] = t4+t5;
+			     b6[k] = t0-t1;
+			     b1[j] = t5-t4;
+			     b2[j] = t2-t7;
+			     b5[k] = t6+t3;
+			     b4[k] = t2+t7;
+			     b3[j] = t3-t6;
 			    }
-			    jr = jr + 2;
-			    ji = ji - 2;
+			    jr += 2;
+			    ji -= 2;
 			    if (ji <= jl){
 			     ji = 2*jr - 1;
 			     jl = jr;
-			    }
-			   }
-			   else {
-			    for (i=0; i<off; i++){
-			     t0 = b0[i] + b1[i];
-			     t1 = b0[i] - b1[i];
-			     t2 = b2[i] * 2.0;
-			     t3 = b3[i] * 2.0;
-			     b0[i] = t0 + t2;
-			     b2[i] = t0 - t2;
-			     b1[i] = t1 + t3;
-			     b3[i] = t1 - t3;
-			    }
-			    if (nn>4) {
-			     k0 = 4*off;
-			     kl = k0 + off;
-			     for (i=k0; i<kl; i++) {
-			      t2 = b0[i] - b2[i];
-			      t3 = b1[i] + b3[i];
-			      b0[i] = (b0[i] + b2[i]) * 2.0;
-			      b2[i] = (b3[i] - b1[i]) * 2.0;
-			      b1[i] = (t2 + t3) * p7two;
-			      b3[i] = (t3 - t2) * p7two;
-			     }
 			    }
 			   }
 			  }
@@ -177,6 +180,5 @@ loop5:	     for (j6=j5; j6<=l6; j6+=l5){
 	 /* } */
 	jj1 += 2;
 	if (jj1 <= l1) goto loop1;
-	/* } */
 }
 
