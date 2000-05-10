3c7e31c68de27bc0aa28fe42baa60b1b5fa5bce2
gibson <gibson>
2000-05-10 21:09:01 +0000

    Revert to v 1.16 (don't exit).

--- src/rtcmix/rtsetinput.cpp
+++ src/rtcmix/rtsetinput.cpp
@@ -66,7 +66,6 @@ rtsetinput(float start_time, Instrument *inst)
       if (inst->inputchans != src_chans) {
          fprintf(stderr, INCHANS_DISCREPANCY_WARNING, inst->inputchans,
                                                                   src_chans);
-         exit(1);
       }
 
       if (inputFileTable[index].is_audio_dev) {
