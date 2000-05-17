91ee9e96426e8d3eb74869cfc965306f6f5603c9
gibson <gibson>
2000-05-17 14:05:04 +0000

    Delete some extern vars that were in ugens.h. Other minor cleanups.

--- src/rtcmix/command_line.c
+++ src/rtcmix/command_line.c
@@ -1,20 +1,17 @@
 /* command_line.c */
 /* to  return command line arguments */
-#include "../H/ugens.h"
-#include "../Minc/defs.h"
+#include <stdlib.h>
+#include <math.h>
+#include <ugens.h>
 #include "../Minc/ext.h"
-extern int aargc;
-extern char *aargv[];          /* to pass commandline args to subroutines */
 
 double f_arg(float *p, short n_args)
 {
-	double atof();
 	return (((int)p[0]) < aargc - 1) ? (atof(aargv[(int)p[0]])) : 0.0;
 }
 
 double i_arg(float *p, short n_args)
 {
-	int atoi();
 	return (((int)p[0]) < aargc - 1) ? (atoi(aargv[(int)p[0]])) : 0;
 }
 
