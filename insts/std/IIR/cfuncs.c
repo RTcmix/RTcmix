7bb76611d2e8ee942ddb3507d513d6b42deac78b
gibson <gibson>
2000-05-17 18:28:13 +0000

    Fix extra carriage return in calls to advise().

--- insts/std/IIR/cfuncs.c
+++ insts/std/IIR/cfuncs.c
@@ -15,14 +15,14 @@ double setup(float *p, int n_args)
 	rsnset(first,bw,1.,0.,rsnetc[0]);
 	advise("IIR setup", "centerfreq    bandwidth  relative amp");
 	amp[0] = p[2];
-	advise(NULL, "            %10.4f %10.4f %10.4f\n",first,bw,amp[0]);
+	advise(NULL, "            %10.4f %10.4f %10.4f",first,bw,amp[0]);
 	for(i=3,j=1; i<n_args; i += 3)  {
 	        if(p[i] < 0.) cf = -p[i] * first;
 	        else  cf = (p[i] < 15.0) ? cpspch(p[i]) : p[i];
 	        bw = p[i+1] < 0 ? -p[i+1] * cf : p[i+1];
 	        amp[j] = p[i+2];
 	        rsnset(cf ,bw ,1.,0.,rsnetc[j]);
-	        advise(NULL, "            %10.4f %10.4f %10.4f\n",cf,bw,amp[j]);
+	        advise(NULL, "            %10.4f %10.4f %10.4f",cf,bw,amp[j]);
 	        j++;
 	}
 	nresons = j;
