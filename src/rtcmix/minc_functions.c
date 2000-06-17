8d88ed3d6f1dc53c79139f3e09730026f76577ce
gibson <gibson>
2000-06-17 15:52:36 +0000

    Minor cleanup: advise() and warn() instead of printf; ansi-fication of some function defs.

--- src/rtcmix/minc_functions.c
+++ src/rtcmix/minc_functions.c
@@ -12,81 +12,62 @@
 #define NUM_ARRAYS  32
 double minc_array[NUM_ARRAYS][ARRAY_SIZE],minc_array_size[NUM_ARRAYS];
 
-double m_boost(p)
-float *p;
+
+double m_boost(float p[])
 { return(boost(p[0])); }
 
-double m_pchmidi(p)
-float *p;
+double m_pchmidi(float p[])
 { return(pchmidi((int)p[0])); }
 
-double m_cpsmidi(p)
-float *p;
+double m_cpsmidi(float p[])
 { return(cpspch(pchmidi(((int)p[0])))); }
 
-double m_midipch(p)
-float *p;
+double m_midipch(float p[])
 { /*printf("%f\n",midipch(p[0]));*/ return(midipch(p[0])); }
 
-double m_cpspch(p)
-float *p;
+double m_cpspch(float p[])
 { return(cpspch(p[0])); }
 
-double m_pchoct(p)
-float *p;
+double m_pchoct(float p[])
 { return(pchoct(p[0])); }
 
-double m_octpch(p)
-float *p;
+double m_octpch(float p[])
 { return(octpch(p[0])); }
 
-double m_pchcps(p)
-float *p;
+double m_pchcps(float p[])
 { return(pchcps(p[0])); }
 
-double m_cpsoct(p)
-float *p;
+double m_cpsoct(float p[])
 { return(cpsoct(p[0])); }
 
-double m_octcps(p)
-float *p;
+double m_octcps(float p[])
 { return(octcps(p[0])); }
 
 double m_rand()
 { float rrand(); return(rrand()); }
 
 double m_random()
-{ float rrand(); return(rrand()*.5+.5); }
+{ float rrand(); return((rrand() * 0.5) + 0.5); }
 
-double m_srand(p)
-float *p;
+double m_srand(float p[])
 { srrand(p[0]); return 0.0; }
 
-double m_time_beat(p)
-float *p;
+double m_time_beat(float p[])
 { float time_beat(float); return(time_beat(p[0])); }
 
-double m_beat_time(p)
-float *p;
+double m_beat_time(float p[])
 { float beat_time(float); return(beat_time(p[0])); }
 
-double m_trunc(p)
-float *p;
-{ return((float)(int)(p[0])); }
+double m_trunc(float p[])
+{ return((double)(int)(p[0])); }
 
-double m_ampdb(p)
-float *p;
+double m_ampdb(float p[])
 { return(ampdb(p[0])); }
 
-double m_dbamp(p)
-float *p;
+double m_dbamp(float p[])
 { return(dbamp(p[0])); }
 
-double
-m_stringify(p,n_args,pp)
-float *p;
-short n_args;
-double *pp;
+double m_stringify(float p, short n_args, double pp[])
 {
 	/* coerces a string passed in from Minc in quotes in p[0]
 	   to a 'floating point' pointer suitable for use in
@@ -95,8 +76,7 @@ double *pp;
 	return(pp[0]);
 }
 
-double
-m_log(float p[], short n_args)
+double m_log(float p[], short n_args)
 {
    double val;
 
@@ -105,31 +85,21 @@ m_log(float p[], short n_args)
    return(val);
 }
 
-double
-m_pow(pp, n_args, p)
-float *pp;
-double *p;
-short n_args;
+double m_pow(float p[], short n_args, double pp[])
 {
 	double val;
 
-	val = pow((double)p[0], (double)p[1]);
+	val = pow(pp[0], pp[1]);
 	return(val);
 }
 
-double
-m_round(p,n_args)
-float *p;
-short n_args;
+double m_round(float p[], int n_args)
 {
 	int val = p[0] + .5;
 	return (double) val;
 }
 
-double
-m_wrap(p,n_args)
-float *p;
-short n_args;
+double m_wrap(float p[], int n_args)
 {
 	/* keeps value between 0 and p[1] */
 	int val = p[0];
@@ -146,30 +116,25 @@ short n_args;
 	return (double) val;
 }
 
-double m_print(float *pp,short n_args,double *p)
+double m_print(float p[], short n_args, double pp[])
 {
-	printf("Value = %10.8f\n", p[0]);
+	printf("Value = %10.8f\n", pp[0]);
 	return 0.0;
 }
 
-double
-m_abs(p, n_args)
-float *p;
-short n_args;
+double m_abs(float p[], int n_args)
 {
-	return((p[0] >= 0.0)?p[0]:-(p[0]));
+	return((p[0] >= 0.0) ? p[0] : -(p[0]));
 }
 
-double m_mod(p,n_args)
-float *p;
+double m_mod(float p[], int n_args)
 {
 	int i,j;
 	i = (int)p[0] % (int)p[1];
 	return((float)i);
 }
 
-double m_max(p,n_args)
-float *p;
+double m_max(float p[], int n_args)
 {
 	int i;
 	float max = -1e+22;
@@ -178,15 +143,13 @@ float *p;
 	return(max);
 }
 
-double m_exit(p,n_args)
-float *p;
+double m_exit(float p[], int n_args)
 {
 	closesf();
 	return 0.0;
 }
 
-double m_load_array(p,n_args, pp)
-float *p;double *pp;
+double m_load_array(float p[], int n_args, double pp[])
 {
 	int i,j;
 	if(n_args > ARRAY_SIZE) n_args = ARRAY_SIZE+1;
@@ -210,8 +173,7 @@ double m_get_array(float p[], int n_args)
 	return (minc_array[i][index]);
 }
 
-double m_put_array(p,n_args)
-float *p;
+double m_put_array(float p[], int n_args)
 { /* to load a single value from minc */
 	int i,j;
 	i=p[0];
@@ -222,8 +184,7 @@ float *p;
 	else return(-1);
 }
 
-double m_get_sum(p,n_args)
-float *p;
+double m_get_sum(float p[], int n_args)
 {
 	int i,j,k;
 	float sum;
@@ -233,18 +194,13 @@ float *p;
 	return(sum);
 }
 
-double m_get_size(p,n_args)
-float *p;
+double m_get_size(float p[], int n_args)
 {
 	/* returns same value as load_array would */
 	return((double)minc_array_size[(int)p[0]]-1);
 }
 
-double
-m_getpch(p,n_args,pp)
-float *p;
-double *pp;
-short n_args;
+double m_getpch(float p[], short n_args, double pp[])
 {
 	int pchfd;
 	int frameno,nbframe,iname;
@@ -257,39 +213,27 @@ short n_args;
 	iname = (int)pp[0];
 	input = (char *)iname;
 
-	if((pchfd = open(input,0)) < 0) {
-		fprintf(stderr," Can't open pitch analysis file\n");
-		exit(-1);
-		}
+	if((pchfd = open(input,0)) < 0)
+		die("getpch", "Can't open pitch analysis file");
 
 	nbframe = 2*FLOAT; 
 	frameno = (int)p[1];
 
 	skipbytes = frameno * nbframe;
-	if(lseek(pchfd, skipbytes, 0) < 0) {
-		printf("error on pchanal lseek\n");
-		exit(-1);
-		}
+	if (lseek(pchfd, skipbytes, 0) < 0)
+		die("getpch", "Error on pchanal lseek");
 
-	if(read(pchfd, (char *)vals, nbframe) != nbframe) {
-			printf("bad read on pchanal file\n");
-			exit(-1);
-			}
-	
+	if (read(pchfd, (char *)vals, nbframe) != nbframe)
+		die("getpch", "Bad read on pchanal file");
+
 	close(pchfd);
 
 	return((double) vals[0]);
 }
 
-
-double
-m_getamp(p,n_args,pp)
-float *p;
-double *pp;
-short n_args;
-
+double m_getamp(float p[], short n_args, double pp[])
 {
-        int pchfd;
+	int pchfd;
 	int frameno,nbframe,iname;
 	long skipbytes;
 	float vals[200]; /* enough for 46 poles + 4 data values */
@@ -300,73 +244,63 @@ short n_args;
 	iname = (int)pp[0];
 	input = (char *)iname;
 
-	if((pchfd = open(input,0)) < 0) {
-		fprintf(stderr," Can't open pitch analysis file\n");
-		exit(-1);
-		}
+	if((pchfd = open(input,0)) < 0)
+		die("getamp", "Can't open pitch analysis file");
 
 	nbframe = 2*FLOAT; 
 	frameno = (int)p[1];
 
 	skipbytes = frameno * nbframe;
-	if(lseek(pchfd, skipbytes, 0) < 0) {
-		printf("error on pchanal lseek\n");
-		exit(-1);
-		}
+	if (lseek(pchfd, skipbytes, 0) < 0)
+		die("getamp", "Error on pchanal lseek");
 
-	if(read(pchfd, (char *)vals, nbframe) != nbframe) {
-			printf("bad read on pchanal file\n");
-			exit(-1);
-			}
-	
+	if (read(pchfd, (char *)vals, nbframe) != nbframe)
+		die("getamp", "Bad read on pchanal file");
+
 	close(pchfd);
 
 	return((double) vals[1]);
 }
 
-
-double str_num(p,n_args,pp)
-float *p; short n_args; double *pp;
+double str_num(float p[], short n_args, double pp[])
 {
-        char *name;
-        int i,j;
-        char buf[16];
-        for(j=0; j<n_args; j=j+2) {
-                buf[0]=0; name=0;
-                i = (int) pp[j];
-                if(((j+1) < (n_args-1)) || !(n_args % 2))
-                        sprintf(buf,"%g",pp[j+1]);
-                name = (char *) i;
-                printf("%s%s",name,buf);
-        }
-        printf("\n");
+	char *name;
+	int i,j;
+	char buf[16];
+
+	for (j=0; j<n_args; j=j+2) {
+		buf[0] = 0;
+		name = 0;
+		i = (int) pp[j];
+		if (((j+1) < (n_args-1)) || !(n_args % 2))
+			sprintf(buf, "%g", pp[j+1]);
+		name = (char *) i;
+		printf("%s%s", name, buf);
+	}
+	printf("\n");
 	return 0.0;
 }
 
-double m_print_is_on(p,n_args)
-float *p;
+double m_print_is_on(float p[], int n_args)
 {
 	print_is_on = 1;
 	return print_is_on;
 }
 
-double m_print_is_off(p,n_args)
-float *p;
+double m_print_is_off(float p[], int n_args)
 {
 	print_is_on = 0;
 	return print_is_on;
 }
 
 struct slist slist[NUM_ARRAYS];
-double m_get_spray(p,n_args)
-float *p;
+double m_get_spray(float p[], int n_args)
 {
 	int i = p[0];
 	return((double)(spray(&slist[i])));
 }
 
-double m_spray_init(p,n_args)
-float *p;
+double m_spray_init(float p[], int n_args)
 {
 	int i,j;
 	i=p[0]; j=p[1];
@@ -379,13 +313,11 @@ static int line_array_size = 1000;      /* modified by m_setline_size */
 
 double m_setline_size(float p[], int n_args)
 {
-	if (p[0] < 2) {
-		fprintf(stderr, "Setline array size must be at least 2!\n");
-		exit(1);
-	}
+	if (p[0] < 2)
+		die("setline_size", "Setline array size must be at least 2!");
 	line_array_size = p[0];
-	printf("Setline arrays will have %d elements.\n", line_array_size);
-
+	advise("setline_size", "Setline arrays will have %d elements.",
+                                                      line_array_size);
 	return 0.0;
 }
 
@@ -416,12 +348,10 @@ int resetval = 1000;                 /* modified by m_reset; read by insts */
 
 double m_reset(float p[], int n_args)
 {
-	if (p[0] <= 0) {
-		fprintf(stderr, "reset must be greater than 0!\n");
-		exit(1);
-	}
+	if (p[0] <= 0)
+		die("reset", "Reset value must be greater than 0!");
 	resetval = p[0];
-	printf("Envelope calls set to %d times per sec.\n", resetval);
+	advise("reset", "Envelope calls set to %d times per sec.", resetval);
 
 	return 0.0;
 }
