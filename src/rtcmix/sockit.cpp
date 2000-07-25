d2cbc79ac450929085262c7c88af452cda9e111b
topper <topper>
2000-07-25 23:41:31 +0000

    Debug print changes

--- src/rtcmix/sockit.cpp
+++ src/rtcmix/sockit.cpp
@@ -58,7 +58,9 @@ extern "C" {
     // timerclear(&tv);
     /* create the socket for listening */
 
+#ifdef DBUG
     cout << "ENTERING sockit() FUNCTION **********\n";
+#endif
     if( (s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
       perror("socket");
       exit(1);
@@ -98,20 +100,21 @@ extern "C" {
       // Zero the socket structure
       sinfo->name[0] = '\0';
       for (i=0;i<MAXDISPARGS;i++) {
-	sinfo->data.p[i] = 0;
+		sinfo->data.p[i] = 0;
       }
 	  
       // we do this when the -n flag is set, it has to parse rtsetparams()
       // coming over the socket before it can access the values of RTBUFSAMPS,
       // SR, NCHANS, etc.
       if (noParse) {
-	cout << "sockit():  waiting for audio config\n";
+		if (print_is_on)
+		  cout << "sockit():  waiting for audio config\n";
 
-	sptr = (char *)sinfo;
-	amt = read(ns, (void *)sptr, sizeof(struct sockdata));
-	while (amt < sizeof(struct sockdata)) amt += read(ns, (void *)(sptr+amt), sizeof(struct sockdata)-amt);
+		sptr = (char *)sinfo;
+		amt = read(ns, (void *)sptr, sizeof(struct sockdata));
+		while (amt < sizeof(struct sockdata)) amt += read(ns, (void *)(sptr+amt), sizeof(struct sockdata)-amt);
 	
-	parse_dispatch(sinfo->name, sinfo->data.p, sinfo->n_args);
+		parse_dispatch(sinfo->name, sinfo->data.p, sinfo->n_args);
       }
 
       buftime = (float)RTBUFSAMPS/SR;
@@ -119,77 +122,79 @@ extern "C" {
       // Main socket reading loop
       while(1) {
 
-	sptr = (char *)sinfo;
-	amt = read(ns, (void *)sptr, sizeof(struct sockdata));
-	while (amt < sizeof(struct sockdata)) amt += read(ns, (void *)(sptr+amt), sizeof(struct sockdata)-amt);
+		sptr = (char *)sinfo;
+		amt = read(ns, (void *)sptr, sizeof(struct sockdata));
+		while (amt < sizeof(struct sockdata)) amt += read(ns, (void *)(sptr+amt), sizeof(struct sockdata)-amt);
 
-	if ( (strcmp(sinfo->name, "rtinput") == 0) || (strcmp(sinfo->name, "rtoutput") == 0) || (strcmp(sinfo->name,"set_option") == 0) || (strcmp(sinfo->name, "load")==0) ) {
-	  // these two commands use text data
-	  // replace the text[i] with p[i] pointers
-	  for (i = 0; i < sinfo->n_args; i++)
-	    strcpy(ttext[i],sinfo->data.text[i]);
-	  for (i = 0; i < sinfo->n_args; i++) {
-	    tmpint = (int)ttext[i];
-	    sinfo->data.p[i] = (double)tmpint;
-	  }
-	}
+		if ( (strcmp(sinfo->name, "rtinput") == 0) || (strcmp(sinfo->name, "rtoutput") == 0) || (strcmp(sinfo->name,"set_option") == 0) || (strcmp(sinfo->name, "load")==0) ) {
+		  // these two commands use text data
+		  // replace the text[i] with p[i] pointers
+		  for (i = 0; i < sinfo->n_args; i++)
+			strcpy(ttext[i],sinfo->data.text[i]);
+		  for (i = 0; i < sinfo->n_args; i++) {
+			tmpint = (int)ttext[i];
+			sinfo->data.p[i] = (double)tmpint;
+		  }
+		}
 
-	if ( (strcmp(sinfo->name, "RTcmix_off") == 0) ) {
-	  rtInteractive = 0;
-	}
+		if ( (strcmp(sinfo->name, "RTcmix_off") == 0) ) {
+		  rtInteractive = 0;
+		}
 	  
-	// if it is an rtupdate, set the pval array
-	if (strcmp(sinfo->name, "rtupdate") == 0) {
-				// rtupdate params are:
-				//	p0 = note tag # 0 for all notes
-				//	p1,2... pn,pn+1 = pfield, value
-	  ntag = (int)sinfo->data.p[0];
-	  pthread_mutex_lock(&pfieldLock);
-	  for (i = 1; i < sinfo->n_args; i += 2) {
-	    pval = (int)sinfo->data.p[i];
-	    pupdatevals[ntag][pval] = sinfo->data.p[i+1];
-	  }
-	  pthread_mutex_unlock(&pfieldLock);
-	  tag_sem=1;
-	}
+		// if it is an rtupdate, set the pval array
+		if (strcmp(sinfo->name, "rtupdate") == 0) {
+		  // rtupdate params are:
+		  //	p0 = note tag # 0 for all notes
+		  //	p1,2... pn,pn+1 = pfield, value
+		  ntag = (int)sinfo->data.p[0];
+		  pthread_mutex_lock(&pfieldLock);
+		  for (i = 1; i < sinfo->n_args; i += 2) {
+			pval = (int)sinfo->data.p[i];
+			pupdatevals[ntag][pval] = sinfo->data.p[i+1];
+		  }
+		  pthread_mutex_unlock(&pfieldLock);
+		  tag_sem=1;
+		}
 
-	else {
+		else {
 	
-	  gettimeofday(&tv, &tz);
-	  sec = (double)tv.tv_sec;
-	  usec = (double)tv.tv_usec;
-	  schedtime = (((sec * 1e6) + usec) - baseTime) * 1e-6;
-	  schedtime += ((double)elapsed/(double)SR);
-	  schedtime += buftime;
+		  gettimeofday(&tv, &tz);
+		  sec = (double)tv.tv_sec;
+		  usec = (double)tv.tv_usec;
+		  schedtime = (((sec * 1e6) + usec) - baseTime) * 1e-6;
+		  schedtime += ((double)elapsed/(double)SR);
+		  schedtime += buftime;
 	  
 #ifdef DBUG
-	  cout << "sockit(): schedtime = " << schedtime << endl;
-	  cout << "sockit(): buftime = " << buftime << endl;
-	  cout << "sockit(): baseTime = " << baseTime << endl;
-	  cout << "sockit(): elapsed = " << elapsed << endl;
-	  cout << "sockit(): SR = " << SR << endl;
+		  cout << "sockit(): schedtime = " << schedtime << endl;
+		  cout << "sockit(): buftime = " << buftime << endl;
+		  cout << "sockit(): baseTime = " << baseTime << endl;
+		  cout << "sockit(): elapsed = " << elapsed << endl;
+		  cout << "sockit(): SR = " << SR << endl;
 #endif
 	  
-	  // schedtime is accessed in rtsetoutput() to
-	  // set the current time.  Plus, in interactive
-	  // mode we have to run a slight delay from
-	  // "0" or we wind up scheduling events in the past.
+		  // schedtime is accessed in rtsetoutput() to
+		  // set the current time.  Plus, in interactive
+		  // mode we have to run a slight delay from
+		  // "0" or we wind up scheduling events in the past.
 	  
-	  if(sinfo->name) {
+		  if(sinfo->name) {
 #ifdef ALLBUG
-	    cout << "SOCKET RECIEVED\n";
-	    cout << "sinfo->name = " << sinfo->name << endl;
-	    cout << "sinfo->n_args = " << sinfo->n_args << endl;
-	    for (i=0;i<sinfo->n_args;i++) {
-	      cout << "sinfo->data.p[" << i << "] =" << sinfo->data.p[i] << endl;
-	    }
+			cout << "SOCKET RECIEVED\n";
+			cout << "sinfo->name = " << sinfo->name << endl;
+			cout << "sinfo->n_args = " << sinfo->n_args << endl;
+			for (i=0;i<sinfo->n_args;i++) {
+			  cout << "sinfo->data.p[" << i << "] =" << sinfo->data.p[i] << endl;
+			}
 #endif
-	    parse_dispatch(sinfo->name, sinfo->data.p, sinfo->n_args);
+			parse_dispatch(sinfo->name, sinfo->data.p, sinfo->n_args);
 	    
-	  }
-	}
+		  }
+		}
       }
     }
+#ifdef DBUG
     cout << "EXITING sockit() FUNCTION **********\n";
+#endif
   }
 }
