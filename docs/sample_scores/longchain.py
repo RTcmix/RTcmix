e132b09d73164fb2fbf52790be773bebed03bb3b
gibson <gibson>
2001-04-15 16:37:50 +0000

    New file.

---
+++ docs/sample_scores/longchain.py
@@ -0,0 +1,116 @@
+# Quick translation of sample_scos_3.0/LONGCHAIN_1.sco to Python, for testing.
+
+# This score makes a wavetable synth riff and feeds it through 3 effects
+# in series: flange -> delay -> reverb
+
+from rt import *
+
+print_off()
+rtsetparams(44100, 2)
+load("WAVETABLE")
+load("FLANGE")
+load("JDELAY")
+load("REVERBIT")
+
+bus_config("WAVETABLE", "aux 0-1 out")
+bus_config("FLANGE", "aux 0-1 in", "aux 10-11 out")
+bus_config("JDELAY", "aux 10-11 in", "aux 4-5 out")
+bus_config("REVERBIT", "aux 4-5 in", "out 0-1")
+
+totdur = 30
+masteramp = 1.0
+atk = 2; dcy = 4
+
+notes = [5.00, 5.001, 5.02, 5.03, 5.05, 5.07, 5.069, 5.10, 6.00]
+numnotes = len(notes)
+
+transposition = 2.00    # try 7.00 also, for some cool aliasing...
+srand(2)
+
+
+# ---------------------------------------------------------------- synth ---
+notedur = .10
+incr = notedur + .015
+
+maxampdb = 92
+minampdb = 75
+ampdiff = maxampdb - minampdb
+
+control_rate(20000)         # need high control rate for short synth notes
+setline(0,0, 1,1, 20,0)
+makegen(2, 10, 10000, 1, .9, .7, .5, .3, .2, .1, .05, .02)
+
+st = 0
+while st < totdur:
+   slot = int(random() * numnotes * .999999)
+   pitch = pchoct(octpch(notes[slot]) + octpch(transposition))
+   amp = ampdb(minampdb + (ampdiff * random()))
+   pctleft = random()
+   WAVETABLE(st, notedur, amp, pitch, pctleft)
+   st = st + incr
+
+
+# for the rest
+control_rate(500)
+amp = masteramp
+
+# --------------------------------------------------------------- flange ---
+resonance = 0.3
+lowpitch = 5.00
+moddepth = 90
+modspeed = 0.08
+wetdrymix = 0.5
+flangetype = 0
+
+gensize = 100000
+makegen(2,10,gensize, 1)
+
+maxdelay = 1.0 / cpspch(lowpitch)
+
+setline(0,1,1,1)
+
+st = 0; insk = 0; inchan = 0; pctleft = 1
+FLANGE(st, insk, totdur, amp, resonance, maxdelay, moddepth,
+       modspeed, wetdrymix, flangetype, inchan, pctleft)
+
+lowpitch = lowpitch + .07
+maxdelay = 1.0 / cpspch(lowpitch)
+
+makegen(2,9,gensize, 1,1,-180)
+
+st = 0; insk = 0; inchan = 1; pctleft = 0
+FLANGE(st, insk, totdur, amp, resonance, maxdelay, moddepth,
+       modspeed, wetdrymix, flangetype, inchan, pctleft)
+
+
+# ---------------------------------------------------------------- delay ---
+deltime = notedur * 2.2
+regen = 0.70
+wetdry = 0.12
+cutoff = 0
+ringdur = 2.0
+
+setline(0,0, atk,1, totdur-dcy,1, totdur,0)
+
+st = 0; insk = 0; inchan = 0; pctleft = 1
+JDELAY(st, insk, totdur, amp, deltime, regen, ringdur, cutoff,
+       wetdry, inchan, pctleft)
+st = 0.02; inchan = 1; pctleft = 0
+JDELAY(st, insk, totdur, amp, deltime, regen, ringdur, cutoff,
+       wetdry, inchan, pctleft)
+
+
+# --------------------------------------------------------------- reverb ---
+revtime = 1.0
+revpct = .3
+rtchandel = .05
+cf = 0
+
+setline(0,1, 1,1)
+
+st = 0; insk = 0
+REVERBIT(st, insk, totdur+ringdur, amp, revtime, revpct, rtchandel, cf)
+
+
+
+# john gibson, 13-feb-01
---
+++ src/parser/parse_with_python.c
@@ -0,0 +1,74 @@
+/* RTcmix  - Copyright (C) 2001  The RTcmix Development Team
+   See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
+   the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
+*/
+
+/* Functions for managing embedded Python parser.    -JGG, 12-Feb-01 */
+
+#include <assert.h>
+#include <Python.h>
+#include "../Python/rtcmix_python_ext.h"
+#include "rtcmix_parse.h"
+
+static FILE *script = NULL;
+
+
+/* ---------------------------------------------------------- parse_score --- */
+int
+parse_score(int argc, char *argv[])
+{
+   int   i, status, xargc;
+   char  *xargv[MAXARGS + 2];
+
+   assert(argc <= MAXARGS);
+
+   Py_SetProgramName(argv[0]);
+
+   /* Init Python interpreter.  If this fails, it won't return. */
+   Py_Initialize();
+
+   /* Define sys.argv in Python. */
+   PySys_SetArgv(argc, argv);
+
+   /* Init our static module (../Python/rtcmix_python_ext.o). */
+   status = init_rtcmix_python_ext();
+
+   if (script == NULL)
+      script = stdin;
+   /* Otherwise, <script> will have been set by use_script_file. */
+
+   /* Run the Python interpreter. */
+   if (status == 0) {
+      PyRun_AnyFile(script, "");    // FIXME: 2nd arg should be script name
+
+      /* Kill interpreter, so that it won't trap cntl-C while insts play. */
+      Py_Finalize();                /* any errors ignored internally */
+   }
+   else
+      fprintf(stderr, "Can't create Python interpreter.\n");
+
+   return status;
+}
+
+
+/* ------------------------------------------------------ use_script_file --- */
+/* Parse file <fname> instead of stdin. */
+void
+use_script_file(char *fname)
+{
+   script = fopen(fname, "r+");
+   if (script == NULL) {
+      fprintf(stderr, "Can't open %s\n", fname);
+      exit(1);
+   }
+   printf("Using file %s\n", fname);
+}
+
+
+/* ------------------------------------------------------- destroy_parser --- */
+void
+destroy_parser()
+{
+   /* nothing to do (see Py_Finalize() above) */
+}
+
