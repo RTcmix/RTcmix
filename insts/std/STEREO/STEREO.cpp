330fd7f2f14e6d294953bff36f6cd1c7bb7834c5
gibson <gibson>
2001-11-10 15:54:30 +0000

    Channel-handling improvements.

--- insts/std/STEREO/STEREO.cpp
+++ insts/std/STEREO/STEREO.cpp
@@ -9,6 +9,15 @@
 #include <rt.h>
 #include <rtdefs.h>
 
+/* If an input file has 2 chans, but there's only one mix matrix pfield,
+   then the 2nd chan will be panned hard right (since an empty pfield has
+   an implicit value of 0).  But I expect a channel with no matrix assignment
+   to be disabled.  Uncommenting the define below gives an input chan with
+   no explicit pfield a value of -1 in the mix matrix, which disables input
+   from that channel.  This is here because I was bitten by this too many
+   times.   JGG, 11/10/01
+*/
+#define DISABLE_UNASSIGNED_INPUT_CHANS
 
 STEREO::STEREO() : Instrument()
 {
@@ -21,6 +30,8 @@ STEREO::~STEREO()
 }
 
 
+#define MATRIX_PFIELD_OFFSET 4
+
 int STEREO::init(float p[], int n_args)
 {
 // p0 = outsk; p1 = insk; p2 = dur (-endtime); p3 = amp; p4-n = channel mix matrix
@@ -31,6 +42,9 @@ int STEREO::init(float p[], int n_args)
 	if (outputchans != 2)
 		die("STEREO", "Output must be stereo.");
 
+	if (n_args <= MATRIX_PFIELD_OFFSET)
+		die("STEREO", "You need at least one channel assignment.");
+
 	if (p[2] < 0.0) p[2] = -p[2] - p[1];
 
 	nsamps = rtsetoutput(p[0], p[2], this);
@@ -39,8 +53,15 @@ int STEREO::init(float p[], int n_args)
 	amp = p[3];
 
 	for (i = 0; i < inputchans; i++) {
-		outspread[i] = p[i+4];
+		outspread[i] = p[i + MATRIX_PFIELD_OFFSET];
 		}
+
+#ifdef DISABLE_UNASSIGNED_INPUT_CHANS
+	/* disable input chans that don't have explicit matrix assignments */
+	i = n_args - MATRIX_PFIELD_OFFSET;
+	for ( ; i < inputchans; i++)
+		outspread[i] = -1.0;
+#endif
 
 	amptable = floc(1);
 	if (amptable) {
