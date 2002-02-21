66ea816dfcfaad151fa186cfb476d1b325f8d5e4
gibson <gibson>
2002-02-21 01:59:11 +0000

    New file.

---
+++ docs/pod/print_rtcmix_vers.pl
@@ -0,0 +1,20 @@
+$cmixdir = $ARGV[0];
+$vers_h = $cmixdir . "/H/version.h";
+
+open VERS_H, $vers_h or die "Can't open $vers_h ($!)";
+
+while (<VERS_H>) {
+   chomp;
+   if (/RTCMIX_NAME/) {
+      s/.*"(.*)"/$1/;
+      $progname = $_;
+   }
+   elsif (/RTCMIX_VERSION/) {
+      s/.*"(.*)"/$1/;
+      $version = $_;
+      break;
+   }
+}
+
+print "$progname v$version\n";
+
