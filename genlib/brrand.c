80c27d5f07f9423396bee756c5cec8a31f7926ea
dscott <dscott>
2002-03-08 05:36:03 +0000

    Minor changes

--- genlib/brrand.c
+++ genlib/brrand.c
@@ -3,7 +3,7 @@
 /* a modification of unix rand() to return floating point values between
    + and - 1. */
 
-static	long	randx = 1;
+static	int	randx = 1;
 
 void
 sbrrand(unsigned int x)
@@ -14,9 +14,9 @@ sbrrand(unsigned int x)
 void
 brrand(float amp, float *a, int j)
 {
-	int k;
+	int i, k;
 	for(k=0; k<j; k++) {
-		int i = ((randx = randx*1103515245 + 12345)>>16) & 077777;
-		*a++ = amp * ((float)i/16384. - 1.);
+		i = ((randx = randx*1103515245 + 12345)>>16) & 077777;
+		a[k] = amp * ((float)i/16384.0f - 1.0f);
 	}
 }
