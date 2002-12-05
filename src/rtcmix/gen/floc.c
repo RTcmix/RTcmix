1a47350a43ab0549f6da146b87293c4c29f269a6
gibson <gibson>
2002-12-05 04:32:28 +0000

    No longer exits when gen is missing.

--- src/rtcmix/gen/floc.c
+++ src/rtcmix/gen/floc.c
@@ -6,32 +6,18 @@ extern float *farrays[];
 extern int sizeof_farray[];
 extern int f_goto[];
 
-/* Returns the address of function number genno.
-   If genno is 1, then returns NULL if that function array doesn't exist.
-   This is part of the new setline scheme:
-      m_setline uses genno 1 in its call to makegen, and
-      insts assume that genno 1 contains the overall amplitude envelope.
-      If genno 1 doesn't exist, insts use 1.0 as an amp multiplier, rather
-        than grabbing one out of a function table. (No more lineset flag.)
-   If genno is not 1 and doesn't exist, complains and bails out.
+/* Returns the address of function number genno, or NULL if the
+   function array doesn't exist.
 
-   NOTE: Some insts don't use genno 1 as an amp envelope, so they need to
-   check the return of floc and complain themselves if it's NULL.
+   NOTE: It's the responsiblity of instruments to deal with a
+   missing gen, either by using die() or supplying a default
+   and alerting the user with advise().
 */
-
 float *floc(int genno)
 {
    int index = f_goto[genno];
-   int size = sizeof_farray[index];
-
-   if (size == 0) {
-      if (genno == 1)
-         return NULL;
-      else {
-         fprintf(stderr, "You haven't allocated function %d yet!\n", genno);
-         closesf();
-      }
-   }
+   if (sizeof_farray[index] == 0)
+      return NULL;
    return farrays[index];
 }
 
