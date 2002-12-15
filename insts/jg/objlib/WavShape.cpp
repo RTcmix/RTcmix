813e899550154a63e14cbdfa50d01231dfb0377c
gibson <gibson>
2002-12-15 21:26:16 +0000

    Better precision to prevent spurious zeros in output.

--- insts/jg/objlib/WavShape.cpp
+++ insts/jg/objlib/WavShape.cpp
@@ -35,22 +35,23 @@ void WavShape :: setTransferFunc(MY_FLOAT *aFunc, int aSize)
 MY_FLOAT WavShape :: tick(MY_FLOAT sample)
 {
    int      loc1;
-   MY_FLOAT findex, frac, val1, val2;
+   MY_FLOAT val1, val2;
+   double   findex, frac;
 
    if (transferFunc) {
-      findex = (sample + 1.0) * indexFactor;
+      findex = (double)((sample + 1.0) * indexFactor);
       if (findex < 0.0)
          findex = 0.0;
 
       loc1 = (int)findex;
-      frac = findex - (MY_FLOAT)loc1;
+      frac = findex - (double)loc1;
       if (loc1 < lastIndex) {
          val1 = transferFunc[loc1];
          val2 = transferFunc[loc1 + 1];
       }
       else     // pin to last array value if sample out of range
          val2 = val1 = transferFunc[lastIndex];
-      lastOutput = val1 + frac * (val2 - val1);
+      lastOutput = val1 + (frac * (val2 - val1));
    }
    else
       lastOutput = sample;
