e5344bc461dd2ec603ed05720ff176259dd7edeb
dscott <dscott>
2003-02-15 01:59:42 +0000

    Optimized loops

--- insts/std/PVOC/overlapadd.c
+++ insts/std/PVOC/overlapadd.c
@@ -4,16 +4,16 @@
  * input I is a folded spectrum of length N; output O and
  * synthesis window W are of length Nw--overlap-add windowed,
  * unrotated, unfolded input data into output O
+ * Since we know N is a power of 2, we can use a mask to wrap n.
  */
 void overlapadd( float I[], int N, float W[], float O[], int Nw, int n )
 {
- int i ;
+	int i;
+	register unsigned mask = (unsigned) N - 1;
     while ( n < 0 )
-	n += N ;
-    n %= N ;
-    for ( i = 0 ; i < Nw ; i++ ) {
-	O[i] += I[n]*W[i] ;
-	if ( ++n == N )
-	    n = 0 ;
+		n += N;
+    n %= N;
+    for (i = 0; i < Nw; ++i, ++n) {
+		O[i] += I[n & mask] * W[i];
     }
 }
