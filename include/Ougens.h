21eddea6602bbd3a11e18dbe57bd1e5d2d01bd12
garton <garton>
2003-02-22 18:44:03 +0000

    added/commited files for new Orand object-ugen

--- include/Ougens.h
+++ include/Ougens.h
@@ -1,10 +1,30 @@
 class Ooscili
 {
-	float si, phase, *array;
+	float si, phase, *array, dur;
 	int length;
 
 public:
 	Ooscili(float, int);
+	Ooscili(float, float*);
+	Ooscili(float, float*, int);
 	float next();
+	float next(int);
 	void setfreq(float);
+	void setphase(float);
+	int getlength();
+	float getdur();
+};
+
+class Orand
+{
+	long rand_x;
+
+public:
+	Orand();
+	Orand(int);
+	void seed(int);
+	void timeseed();
+	float random();
+	float rand();
+	float range(float, float);
 };
