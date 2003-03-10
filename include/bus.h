c82fb6cd16fe310b6a8c2850cec4603950840d9d
dscott <dscott>
2003-03-10 22:41:15 +0000

    Moved MAXBUS to globals.h to reduce dependency on this header

--- include/bus.h
+++ include/bus.h
@@ -1,42 +1,62 @@
 #ifndef _BUS_H_ 
 #define _BUS_H_ 1
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+#include <globals.h>
 
-#define MAXBUS 16
-
-typedef enum {
+enum ErrCode {
    NO_ERR = 0,
    INVAL_BUS_ERR,
    INVAL_BUS_CHAN_ERR,
    LOOP_ERR,
    UNKNOWN_ERR
-} ErrCode;
+};
 
-typedef enum {
+enum BusType {
    BUS_IN,
    BUS_OUT,
    BUS_AUX_IN,
    BUS_AUX_OUT
-} BusType;
-
-typedef struct _BusSlot BusSlot;
-
-struct _BusSlot {
-   BusSlot    *next;
-   BusSlot    *prev;
-   short      in_count;
-   short      in[MAXBUS];
-   short      out_count;
-   short      out[MAXBUS];
-   short      auxin_count;
-   short      auxin[MAXBUS];
-   short      auxout_count;
-   short      auxout[MAXBUS];
-   int        refcount;
 };
+
+enum IBusClass {
+  TO_AUX,
+  AUX_TO_AUX,
+  TO_OUT,
+  TO_AUX_AND_OUT,
+  UNKNOWN
+};
+
+struct BusSlot {
+	BusSlot();
+	inline IBusClass Class() const;
+	BusSlot    *next;
+	BusSlot    *prev;
+	short      in_count;
+	short      in[MAXBUS];
+	short      out_count;
+	short      out[MAXBUS];
+	short      auxin_count;
+	short      auxin[MAXBUS];
+	short      auxout_count;
+	short      auxout[MAXBUS];
+	int        refcount;
+};
+
+inline IBusClass 
+BusSlot::Class() const
+{
+  if (this == 0)
+	return UNKNOWN;
+  if (auxin_count > 0 && auxout_count > 0)
+	return AUX_TO_AUX;
+  if (auxout_count > 0 && out_count > 0)
+	return TO_AUX_AND_OUT;
+  if (auxout_count > 0)
+	return TO_AUX;
+  if (out_count > 0)
+	return TO_OUT;
+  return UNKNOWN;
+}
 
 typedef struct _BusQueue BusQueue;
 
@@ -60,6 +80,9 @@ struct _CheckQueue {
    CheckQueue *next;
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 /* exported functions */
 BusSlot *get_bus_config(const char *inst_name);
