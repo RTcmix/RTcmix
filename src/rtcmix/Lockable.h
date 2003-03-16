50423ee1c8b048e44414f6be22fbcb9e8866e9ff
dscott <dscott>
2003-03-16 17:40:52 +0000

    New base class for all objects which need thread locking.

---
+++ src/rtcmix/Lockable.h
@@ -0,0 +1,27 @@
+// Lockable.h
+//
+// Base class for all classes which support mutex locking.
+// Class is entirely inline, so no .C source.
+//
+
+#ifndef _RT_LOCKABLE_H_
+#define _RT_LOCKABLE_H_
+
+#include <globals.h>
+#include <lock.h>
+
+class Lockable {
+public:
+	Lockable() { pthread_mutex_init(&_mutex, NULL); }
+	~Lockable() { pthread_mutex_destroy(&_mutex); }
+	void		lock() { pthread_mutex_lock(&_mutex); }
+	void		unlock() { pthread_mutex_unlock(&_mutex); }
+protected:
+	// This allows subclasses to create temp Lock class instances
+	LockHandle	getlockhandle() { return (LockHandle) &_mutex; }
+private:
+	pthread_mutex_t _mutex;
+};
+
+#endif	//	 _RT_LOCKABLE_H_
+
