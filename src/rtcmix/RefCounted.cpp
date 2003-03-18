525c7f6f932bbcc369d6cb2c9316a2f492a25c8d
dscott <dscott>
2003-03-18 00:22:37 +0000

    Added static RefCounted::Unref() method

--- src/rtcmix/RefCounted.cpp
+++ src/rtcmix/RefCounted.cpp
@@ -5,3 +5,10 @@
 
 RefCounted::~RefCounted() {}
 
+// This is used to unref pointers which may be NULL without needed to do the
+//	null check every time.  Just call RefCounted::Unref(ptr).
+
+int RefCounted::Unref(RefCounted *r)
+{
+	return (r) ? r->Unref() : 0;
+}
--- src/rtcmix/RefCounted.h
+++ src/rtcmix/RefCounted.h
@@ -9,10 +9,11 @@
 class RefCounted {
 public:
 	int Ref() { return ++_refcount; }
-	int Unref() { int r; if ((r=--_refcount) == 0) { delete this; } return r; }
+	int Unref() { int r; if ((r=--_refcount) <= 0) { delete this; } return r; }
+	static int Unref(RefCounted *r);	// defined in rtstuff/RefCounted.C
 protected:
 	RefCounted() : _refcount(0) {}
-	virtual ~RefCounted();	// defined in rtstuff/RefCounted.C
+	virtual ~RefCounted();				// defined in rtstuff/RefCounted.C
 private:
 	int _refcount;
 };
