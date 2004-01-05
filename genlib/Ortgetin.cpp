c46405c986d36ae4e7588ebea3a9fefaf6a8ab41
garton <garton>
2004-01-05 04:05:52 +0000

    *** empty log message ***

---
+++ genlib/Ortgetin.cpp
@@ -0,0 +1,32 @@
+#include <ugens.h>
+#include "../H/Ougens.h"
+
+Ortgetin::Ortgetin(Instrument *ins)
+{
+	int i;
+
+	theInst = ins;
+	chns = theInst->inputChannels();
+	rsamps = RTBUFSAMPS*chns;
+	in = new float[rsamps];
+
+	// force a read for the very first access
+	chptr = rsamps;
+}
+
+int Ortgetin::next(float *inarr)
+{
+	int nread;
+	int i;
+
+	chptr += chns;
+	if (chptr >= rsamps)
+	{
+		nread = theInst->rtgetin(in, theInst, rsamps);
+		chptr = 0;
+	}
+
+	for (i = 0; i < chns; i++) inarr[i] = in[chptr+i];
+
+	return(nread);
+}
