61a9f1acb5e339f27c8580b020da42bb3f9b659b
dscott <dscott>
2004-05-05 23:53:41 +0000

    Simplified considerably, but still has a hack.

--- src/rtcmix/rtgetsamps.cpp
+++ src/rtcmix/rtgetsamps.cpp
@@ -14,50 +14,19 @@
 #include "../rtstuff/rtdefs.h"
 #include "AudioDevice.h"
 
-extern AudioDevice *globalInputDevice;	// from audio_devices.cpp
-
-/* local prototypes */
-
-static int read_from_audio_device(void);
-
-
-/* ----------------------------------------------- read_from_audio_device --- */
-static int
-read_from_audio_device(AudioDevice *inputDevice)
-{
-	int result;
-
-	result = inputDevice->getFrames(audioin_buffer, RTBUFSAMPS);
-	if (result == -1) {
-		fprintf(stderr, "read_from_audio_device got error: %s\n",
-				inputDevice->getLastError());
-		return -1;
-	}
-
-   return 0;
-}
-
+extern AudioDevice *globalAudioDevice;	// audio_devices.cpp
 
 /* ----------------------------------------------------------- rtgetsamps --- */
 void
 rtgetsamps(AudioDevice *inputDevice)
 {
-   int   err;
-
-   assert(audio_on == 1);
-
-	// This is a hack:  until I can create an AudioDevice which is guaranteed
-	// to work in full duplex, we need to override the device handed to us to
-	// point to the global input device.  Otherwise, we will be calling read on
-	// a device possibly configured only for output (playback).
-
-	if (!globalInputDevice || globalInputDevice == inputDevice)
-	   err = read_from_audio_device(inputDevice);
-	else
-	   err = read_from_audio_device(globalInputDevice);
-
-   if (err)
-      fprintf(stderr, "rtgetsamps: bad read from audio device\n");
+	assert(audio_on == 1);
+	// Sorrowful hack:  The dual AudioDevice run the callback on the playback
+	// device, which means it cannot pass the record device to this function.
+	if (globalAudioDevice->getFrames(audioin_buffer, RTBUFSAMPS) < 0)
+	{
+		fprintf(stderr, "rtgetsamps error: %s\n", globalAudioDevice->getLastError());
+	}
 }
 
 
