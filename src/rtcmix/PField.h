fcdf1822f65b082686a155eab5808a75c3f1eb40
gibson <gibson>
2004-06-05 13:31:37 +0000

    Remove void from dump declaration.

--- src/rtcmix/PField.h
+++ src/rtcmix/PField.h
@@ -94,7 +94,7 @@ class TablePField : public RTPField {
 public:
 	TablePField(double *tableArray, int length);
 	virtual double	doubleValue(double) const;
-	char *dump(void) const;
+	char *dump() const;
 protected:
 	virtual ~TablePField();
 private:
