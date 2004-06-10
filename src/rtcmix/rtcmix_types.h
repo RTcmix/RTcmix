54394eee984025e8595aedfc866a8369ad6d093a
dscott <dscott>
2004-06-10 23:07:46 +0000

    Arg struct used in both C and C++ contexts, one as a struct and the other as a class.  Made string pointer const in union.

--- src/rtcmix/rtcmix_types.h
+++ src/rtcmix/rtcmix_types.h
@@ -1,6 +1,8 @@
 #ifndef _RTCMIX_TYPES_H_
 #define _RTCMIX_TYPES_H_ 1
 
+#include <stdio.h>
+
 typedef enum {
    VoidType = 0,
    DoubleType,
@@ -26,15 +28,39 @@ typedef struct {
 } Array;
 
 typedef union {
-   double number;
-   char   *string;
-   Handle handle;
-   Array  *array;
+   double		number;
+   const char	*string;
+   Handle		handle;
+   Array		*array;
 } Value;
 
+// Arg operates as a struct in C, and a class in C++.
+
+#ifdef __cplusplus
+struct Arg {
+#else
 typedef struct {
+#endif
    RTcmixType type;
    Value val;
+#ifdef __cplusplus
+   Arg() : type(VoidType) {}
+   ~Arg();
+   RTcmixType getType() const { return type; }
+   void operator = (double d) { type = DoubleType; val.number = d; }
+   void operator = (const char *c) { type = StringType; val.string = c; }
+   void operator = (const Handle h) { type = HandleType; val.handle = h; }
+   void operator = (Array *a) { type = ArrayType; val.array = a; }
+   operator double () const { return val.number; }
+   operator float () const { return (float) val.number; }
+   operator int () const { return (int) val.number; }
+   operator const char *() const { return val.string; }
+   operator Handle () const { return val.handle; }
+   operator Array *() const { return val.array; }
+   void printInline(FILE *) const;
+};
+#else
 } Arg;
+#endif
 
 #endif /* _RTCMIX_TYPES_H_ */
