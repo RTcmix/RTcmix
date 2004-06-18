b8108a6a461f44ce05800045882fa2430b5685c6
gibson <gibson>
2004-06-18 01:11:03 +0000

    Much revision.

--- test/coreaudioprobe/coreaudioprobe.cpp
+++ test/coreaudioprobe/coreaudioprobe.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <CoreAudio/CoreAudio.h>
 
 const Boolean isOutput = 0;
@@ -37,16 +38,144 @@ const char *errToString(OSStatus err)
 }
 
 
-UInt32
+char *
+GetDeviceName(AudioDeviceID devID)
+{
+   UInt32 size = 0;
+
+   OSStatus err = AudioDeviceGetPropertyInfo(devID,
+                                             0,
+                                             isOutput,  // arbitrary?
+                                             kAudioDevicePropertyDeviceName,
+                                             &size, NULL);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get device name property info for device %d.\n",
+                                                                        devID);
+      return NULL;
+   }
+
+   char *name = new char[size + 1];
+   err = AudioDeviceGetProperty(devID,
+                                0,
+                                isOutput,    // arbitrary?
+                                kAudioDevicePropertyDeviceName,
+                                &size, name);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get device name property for device %d.\n", devID);
+      return NULL;
+   }
+
+   return name;
+}
+
+
+char *
+GetStreamName(AudioDeviceID devID, UInt32 chan, Boolean isInput)
+{
+   UInt32 size = 0;
+
+   OSStatus err = AudioDeviceGetPropertyInfo(devID,
+                                    chan,
+                                    isInput,
+                                    kAudioDevicePropertyChannelCategoryName,
+                                    &size, NULL);
+   if (err != kAudioHardwareNoError) {
+#if 0  // It's okay for a device not to have channel category names.
+      fprintf(stderr,
+              "Can't get channel category name property info for device %d.\n",
+                                                                        devID);
+#endif
+      return NULL;
+   }
+
+   char *name = new char[size + 1];
+   err = AudioDeviceGetProperty(devID,
+                                chan,
+                                isInput,
+                                kAudioDevicePropertyChannelCategoryName,
+                                &size, name);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr,
+            "Can't get channel category name property for device %d.\n", devID);
+      return NULL;
+   }
+
+   return name;
+}
+
+
+char *
+GetStreamChannelName(AudioDeviceID devID, UInt32 chan, Boolean isInput)
+{
+   UInt32 size = 0;
+
+   OSStatus err = AudioDeviceGetPropertyInfo(devID,
+                                             chan,
+                                             isInput,
+                                             kAudioDevicePropertyChannelName,
+                                             &size, NULL);
+   if (err != kAudioHardwareNoError) {
+#if 0  // It's okay for a device not to have channel names.
+      fprintf(stderr, "Can't get channel name property info for device %d.\n",
+                                                                        devID);
+#endif
+      return NULL;
+   }
+
+   char *chanName = new char[size + 1];
+   err = AudioDeviceGetProperty(devID,
+                                chan,
+                                isInput,
+                                kAudioDevicePropertyChannelName,
+                                &size, chanName);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get device name property for device %d.\n", devID);
+      return NULL;
+   }
+
+#ifdef NOTYET // this was returning empty strings for MOTU 828
+   err = AudioDeviceGetPropertyInfo(devID,
+                                    chan,
+                                    isInput,
+                                    kAudioDevicePropertyChannelNumberName,
+                                    &size, NULL);
+   if (err != kAudioHardwareNoError) {
+#if 1  // It's okay for a device not to have channel number names.
+      fprintf(stderr,
+               "Can't get channel number name property info for device %d.\n",
+                                                                        devID);
+#endif
+      return NULL;
+   }
+
+   char *chanNumName = new char[size + 1];
+   err = AudioDeviceGetProperty(devID,
+                                chan,
+                                isInput,
+                                kAudioDevicePropertyChannelNumberName,
+                                &size, chanNumName);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get device name property for device %d.\n", devID);
+      return NULL;
+   }
+
+   printf("chanNumName: %s\n", chanNumName);
+#endif
+
+   return chanName;
+}
+
+
+AudioDeviceID
 GetDefaultDeviceID(Boolean isInput)
 {
-   UInt32 devID;
+   AudioDeviceID devID;
+   AudioDevicePropertyID prop;
 
+   prop = isInput ? kAudioHardwarePropertyDefaultInputDevice
+                  : kAudioHardwarePropertyDefaultOutputDevice;
    UInt32 size = sizeof(devID);
-   OSStatus err = AudioHardwareGetProperty(
-                  kAudioHardwarePropertyDefaultOutputDevice,
-                  &size,
-                  &devID);
+   OSStatus err = AudioHardwareGetProperty(prop, &size, &devID);
    if (err != kAudioHardwareNoError || devID == kAudioDeviceUnknown) {
       fprintf(stderr, "Can't find default %s device: %s\n",
             isInput ? "input" : "output", errToString(err));
@@ -57,13 +186,11 @@ GetDefaultDeviceID(Boolean isInput)
 }
 
 
-UInt32
-PrintBufferList(UInt32 devID, Boolean isInput)
+int
+PrintStreamList(AudioDeviceID devID, Boolean isInput, UInt32 *totChans)
 {
    UInt32 size = 0;
    Boolean writeable = 0;
-
-   printf("\nBuffer List --------------------------------------------------\n");
 
    // Get size of AudioBufferList, in bytes.
    OSStatus err = AudioDeviceGetPropertyInfo(devID,
@@ -74,7 +201,7 @@ PrintBufferList(UInt32 devID, Boolean isInput)
    if (err != kAudioHardwareNoError) {
       fprintf(stderr, "Can't get %s device (%d) property info: %s\n",
             isInput ? "input" : "output", devID, errToString(err));
-      return 0;
+      return -1;
    }
 
 //printf("stream config prop size: %d, writeable: %d\n", size, writeable);
@@ -89,30 +216,30 @@ PrintBufferList(UInt32 devID, Boolean isInput)
    if (err != kAudioHardwareNoError) {
       fprintf(stderr, "Can't get %s device (%d) configuration: %s\n",
             isInput ? "input" : "output", devID, errToString(err));
-      return 0;
+      return -1;
    }
 
-   printf("%s device (%d) has %d buffer%s:\n",
+   printf("   %s device (%d) has %d stream%s:\n",
             isInput ? "Input" : "Output", devID, list->mNumberBuffers,
-            list->mNumberBuffers > 1 ? "s" : "");
+            list->mNumberBuffers == 1 ? "" : "s");
 
    // Print number of channels in each buffer.
-   UInt32 totalChans = 0;
+   *totChans = 0;
    for (UInt32 i = 0; i < list->mNumberBuffers; i++) {
       UInt32 numChans = list->mBuffers[i].mNumberChannels;
-      printf("   Buffer %d has %d channels\n", i, numChans);
-      totalChans += numChans;
+      printf("      Stream %d has %d channels\n", i, numChans);
+      *totChans += numChans;
    }
-   printf("   Total: %d channels\n", totalChans);
+   printf("   Total number of channels: %d\n", *totChans);
 
-   delete list;
+   delete [] list;
 
-   return totalChans;
+   return 0;
 }
 
 
 int
-PrintStreamDescription(UInt32 devID, UInt32 chan, Boolean isInput)
+PrintStreamChannelInformation(AudioDeviceID devID, UInt32 chan, Boolean isInput)
 {
    UInt32 size = sizeof(AudioStreamBasicDescription);
    AudioStreamBasicDescription strDesc;
@@ -138,8 +265,59 @@ PrintStreamDescription(UInt32 devID, UInt32 chan, Boolean isInput)
       return -1;
    }
 
-   printf("The %s stream containing channel %d has %d channels per frame.\n",
-            isInput? "input" : "output", chan, strDesc.mChannelsPerFrame);
+#ifdef NOTYET
+   // this wasn't too informative, but maybe we should investigate it more
+   char *streamName = GetStreamName(devID, chan, isInput);
+   if (streamName) {
+      printf("Stream name: \"%s\"\n", streamName);
+      delete [] streamName;
+   }
+#endif
+
+   char *chanName = GetStreamChannelName(devID, chan, isInput);
+   if (chanName) {
+      if (chan == 1)    // first line for device section
+         printf("   Stream containing chan %d has %d chans per frame (\"%s\")\n",
+               chan, strDesc.mChannelsPerFrame, chanName);
+      else
+         printf("   \"         \"            %d     %d       \"         (\"%s\")\n",
+               chan, strDesc.mChannelsPerFrame, chanName);
+      delete [] chanName;
+   }
+   else {
+      if (chan == 1)
+         printf("   Stream containing chan %d has %d chans per frame\n",
+               chan, strDesc.mChannelsPerFrame);
+      else
+         printf("   \"         \"            %d     %d       \"\n",
+               chan, strDesc.mChannelsPerFrame);
+   }
+
+   return 0;
+}
+
+
+int
+GetDeviceList(AudioDeviceID **devList, int *devCount)
+{
+   UInt32 size;
+
+   OSStatus err = AudioHardwareGetPropertyInfo(
+                        kAudioHardwarePropertyDevices,
+                        &size, NULL);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get hardware device list property info.\n");
+      return -1;
+   }
+   *devCount = size / sizeof(AudioDeviceID);
+   *devList = new AudioDeviceID[*devCount];
+   err = AudioHardwareGetProperty(
+                        kAudioHardwarePropertyDevices,
+                        &size, *devList);
+   if (err != kAudioHardwareNoError) {
+      fprintf(stderr, "Can't get hardware device list.\n");
+      return -1;
+   }
 
    return 0;
 }
@@ -148,25 +326,56 @@ PrintStreamDescription(UInt32 devID, UInt32 chan, Boolean isInput)
 int
 main(int argc, char *argv[])
 {
-   UInt32 devID = GetDefaultDeviceID(isOutput);
-   if (devID == 0)
-      return -1;
-   UInt32 totChans = PrintBufferList(devID, isOutput);
-   if (totChans == 0)
-      return -1;
-   printf("\nStream Description -------------------------------------------\n");
-   for (UInt32 n = 0; n <= totChans; n++)
-      PrintStreamDescription(devID, n, isOutput);
+   AudioDeviceID *devList;
+   int devCount;
 
-   devID = GetDefaultDeviceID(isInput);
-   if (devID == 0)
+   if (GetDeviceList(&devList, &devCount) != 0)
       return -1;
-   totChans = PrintBufferList(devID, isInput);
-   if (totChans == 0)
+   printf("\nNumber of devices: %d\n", devCount);
+
+   AudioDeviceID defaultInDevID = GetDefaultDeviceID(isInput);
+   if (defaultInDevID == 0)
       return -1;
-   printf("\nStream Description -------------------------------------------\n");
-   for (UInt32 n = 0; n <= totChans; n++)
-      PrintStreamDescription(devID, n, isInput);
+   AudioDeviceID defaultOutDevID = GetDefaultDeviceID(isOutput);
+   if (defaultOutDevID == 0)
+      return -1;
+
+   for (int i = 0; i < devCount; i++) {
+      AudioDeviceID devID = devList[i];
+      char *devName = GetDeviceName(devID);
+      if (devName == NULL)
+         return -1;
+      printf("\n");
+      printf("=============================================================\n");
+      printf("Device ID %d: \"%s\"", devID, devName);
+      if (devID == defaultInDevID)
+         printf(" (default input)");
+      if (devID == defaultOutDevID)
+         printf(" (default output)");
+      printf("\n\n");
+
+      printf("Input Section -----------------------------------------------\n");
+      UInt32 totChans;
+      if (PrintStreamList(devID, isInput, &totChans) != 0)
+         return -1;
+      for (UInt32 n = 1; n <= totChans; n++)
+         if (PrintStreamChannelInformation(devID, n, isInput) != 0)
+            return -1;
+      printf("\n");
+
+      printf("Output Section ----------------------------------------------\n");
+      if (PrintStreamList(devID, isOutput, &totChans) != 0)
+         return -1;
+      for (UInt32 n = 1; n <= totChans; n++)
+         if (PrintStreamChannelInformation(devID, n, isOutput) != 0)
+            return -1;
+
+      delete [] devName;
+   }
+
+   delete [] devList;
+
+   printf("\n");
 
    return 0;
 }
