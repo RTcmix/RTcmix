6e15c1004c03c4fcc50ae92aca3138c46b41d7f6
dscott <dscott>
2004-07-04 19:59:46 +0000

    Centralized header for types needed by public headers.

---
+++ include/rt_types.h
@@ -0,0 +1,13 @@
+/* RTcmix  - Copyright (C) 2000  The RTcmix Development Team
+   See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
+   the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
+*/
+
+#ifndef _RT_TYPES_H_
+#define _RT_TYPES_H_
+
+/* type of buffer used for internal buses */
+#define BUFTYPE float           /* could be double some day */
+typedef BUFTYPE *BufPtr;
+
+#endif	// _RT_TYPES_H_
