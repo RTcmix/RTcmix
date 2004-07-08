8b34596ee1efb0df70a581b7aa1e41f0d49bfbc3
gibson <gibson>
2004-07-08 12:48:08 +0000

    Rename more symbols.

--- src/conf/fixlexoutput.sh
+++ src/conf/fixlexoutput.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# Replace all the global var names in lex.conf.c with ones that don't
+# Replace all the global symbols in lex.conf.c with ones that don't
 # conflict with our Minc parser.  The bison-generated source (conf.tab.cpp)
 # uses the macros defined at the top of lex.conf.c to access these
-# variables, so this substitution should not matter to it.   -JGG, 7/7/04
+# variables, so this substitution should not matter to it.  We use a brute
+# force approach, which means that we rename some things that don't
+# have to be renamed, but at least we get all the symbols that could
+# possibly be a problem.                                -JGG, 7/7/04
 
 sed \
--e 's/yyin/__yyin/g' \
--e 's/yyleng/__yyleng/g' \
--e 's/yyout/__yyout/g' \
--e 's/yytext/__yytext/g' \
--e 's/yylineno/__yylineno/g' \
+-e 's/yy/__yy/g' \
+-e 's/__yystype\.h/yystype\.h/g' \
 $1 > $1.tmp
 
 mv $1.tmp $1
