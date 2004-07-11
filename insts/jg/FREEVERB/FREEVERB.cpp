4ae7ebff0b393ad11042951a841ce90ee6805f52
gibson <gibson>
2004-07-11 23:05:53 +0000

    PField updates.

--- insts/jg/FREEVERB/FREEVERB.cpp
+++ insts/jg/FREEVERB/FREEVERB.cpp
@@ -15,9 +15,14 @@
    p9  = wet signal level (0-100%)
    p10 = stereo width of reverb (0-100%)
 
-   Assumes function table 1 is amplitude curve for the note. (Try gen 18.)
-   Or you can just call setline. If no setline or function table 1, uses
-   flat amplitude curve.  The curve is applied to the input sound *before*
+   p3 (amplitude), p4 (room size), p5 (pre-delay), p7 (damp), p8 (dry),
+   p9 (wet) and p10 (stereo width) can receive dynamic updates from a table
+   or real-time control source.
+
+   If an old-style gen table 1 is present, its values will be multiplied
+   by the p3 amplitude multiplier, even if the latter is dynamic.
+
+   The amplitude multiplier is applied to the input sound *before*
    it enters the reverberator.
 
    If you enter a room size greater than the maximum, you'll get the
@@ -28,13 +33,12 @@
    Be careful with the dry and wet levels -- it's easy to get extreme
    clipping!
 
-   John Gibson <johngibson@virginia.edu>, 2 Feb 2001
+   John Gibson <johngibson@virginia.edu>, 2 Feb 2001; rev for v4, 7/11/04
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ugens.h>
 #include <math.h>
-#include <mixerr.h>
 #include <Instrument.h>
 #include "FREEVERB.h"
 #include <rt.h>
@@ -45,6 +49,12 @@ FREEVERB :: FREEVERB() : Instrument()
 {
    in = NULL;
    branch = 0;
+   warn_roomsize = true;
+   warn_predelay = true;
+   warn_damp = true;
+   warn_dry = true;
+   warn_wet = true;
+   warn_width = true;
 }
 
 
@@ -57,14 +67,9 @@ FREEVERB :: ~FREEVERB()
 
 int FREEVERB :: init(double p[], int n_args)
 {
-   float outskip, inskip, dur, roomsize, damp, dry, wet, width, max_roomsize;
-   float predelay_time;
-   int   predelay_samps;
-
-   outskip = p[0];
-   inskip = p[1];
-   dur = p[2];
-   amp = p[3];
+   float outskip = p[0];
+   float inskip = p[1];
+   float dur = p[2];
    roomsize = p[4];
    predelay_time = p[5];
    ringdur = p[6];
@@ -73,7 +78,7 @@ int FREEVERB :: init(double p[], int n_args)
    wet = p[9];
    width = p[10];
 
-   /* Keep reverb comb feedback <= 1.0 */
+   // Keep reverb comb feedback <= 1.0
    max_roomsize = (1.0 - offsetroom) / scaleroom;
    if (roomsize < 0.0)
       return die("FREEVERB", "Room size must be between 0 and %g.",
@@ -83,7 +88,7 @@ int FREEVERB :: init(double p[], int n_args)
       advise("FREEVERB", "Room size cannot be greater than %g. Adjusting...",
              max_roomsize);
    }
-   predelay_samps = (int)((predelay_time * SR) + 0.5);
+   int predelay_samps = (int) ((predelay_time * SR) + 0.5);
    if (predelay_samps > max_predelay_samps)
       return die("FREEVERB", "Pre-delay must be between 0 and %g seconds.",
                                              (float) max_predelay_samps / SR);
@@ -99,11 +104,11 @@ int FREEVERB :: init(double p[], int n_args)
    if (rtsetinput(inskip, this) != 0)
       return DONT_SCHEDULE;
    nsamps = rtsetoutput(outskip, dur + ringdur, this);
-   insamps = (int)(dur * SR);
+   insamps = (int) (dur * SR);
 
-   if (inputchans > 2)
+   if (inputChannels() > 2)
       return die("FREEVERB", "Can't have more than 2 input channels.");
-   if (outputchans > 2)
+   if (outputChannels() > 2)
       return die("FREEVERB", "Can't have more than 2 output channels.");
 
    rvb = new revmodel();
@@ -120,60 +125,141 @@ int FREEVERB :: init(double p[], int n_args)
       int lenamp = fsize(1);
       tableset(dur, lenamp, amptabs);
    }
-   else
-      advise("FREEVERB", "Setting phrase curve to all 1's.");
-
-   aamp = amp;                  /* in case amparray == NULL */
 
    skip = (int) (SR / (float) resetval);
 
-   return nsamps;
+   return nSamps();
+}
+
+
+int FREEVERB :: configure()
+{
+   in = new float [RTBUFSAMPS * inputChannels()];
+   return in ? 0 : -1;
+}
+
+
+inline void FREEVERB :: updateRvb(double p[])
+{
+   if (p[4] != roomsize) {
+      roomsize = p[4];
+      if (roomsize < 0.0 || roomsize > max_roomsize) {
+         if (warn_roomsize) {
+            warn("FREEVERB", "Room size must be between 0 and %g. Adjusting...",
+                                                                  max_roomsize);
+            warn_roomsize = false;
+         }
+         roomsize = roomsize < 0.0 ? 0.0 : max_roomsize;
+      }
+      rvb->setroomsize(roomsize);
+   }
+   if (p[5] != predelay_time) {
+      predelay_time = p[5];
+      int predelay_samps = (int) ((predelay_time * SR) + 0.5);
+      if (predelay_samps > max_predelay_samps) {
+         if (warn_predelay) {
+            warn("FREEVERB", "Pre-delay must be between 0 and %g seconds. "
+                             "Adjusting...", (float) max_predelay_samps / SR);
+            warn_predelay = false;
+         }
+         predelay_samps = max_predelay_samps;
+      }
+      rvb->setpredelay(predelay_samps);
+   }
+   if (p[7] != damp) {
+      damp = p[7];
+      if (damp < 0.0 || damp > 100.0) {
+         if (warn_damp) {
+            warn("FREEVERB", "Damp must be between 0 and 100%%. Adjusting...");
+            warn_damp = false;
+         }
+         damp = damp < 0.0 ? 0.0 : 100.0;
+      }
+      rvb->setdamp(damp * 0.01);
+   }
+   if (p[8] != dry) {
+      dry = p[8];
+      if (dry < 0.0 || dry > 100.0) {
+         if (warn_dry) {
+            warn("FREEVERB", "Dry signal level must be between 0 and 100%%. "
+                                                               "Adjusting...");
+            warn_dry = false;
+         }
+         dry = dry < 0.0 ? 0.0 : 100.0;
+      }
+      rvb->setdry(dry * 0.01);
+   }
+   if (p[9] != wet) {
+      wet = p[9];
+      if (wet < 0.0 || wet > 100.0) {
+         if (warn_wet) {
+            warn("FREEVERB", "Wet signal level must be between 0 and 100%%. "
+                                                               "Adjusting...");
+            warn_wet = false;
+         }
+         wet = wet < 0.0 ? 0.0 : 100.0;
+      }
+      rvb->setwet(wet * 0.01);
+   }
+   if (p[10] != width) {
+      width = p[10];
+      if (width < 0.0 || width > 100.0) {
+         if (warn_width) {
+            warn("FREEVERB", "Width must be between 0 and 100%%. Adjusting...");
+            warn_width = false;
+         }
+         width = width < 0.0 ? 0.0 : 100.0;
+      }
+      rvb->setwidth(width * 0.01);
+   }
 }
 
 
 int FREEVERB :: run()
 {
-   int   samps;
    float *inL, *inR, *outL, *outR;
 
-   if (in == NULL)
-      in = new float [RTBUFSAMPS * inputchans];
-
    inL = in;
-   inR = inputchans > 1 ? in + 1 : in;
+   inR = inputChannels() > 1 ? in + 1 : in;
    outL = outbuf;
-   outR = outputchans > 1 ? outbuf + 1 : outbuf;
+   outR = outputChannels() > 1 ? outbuf + 1 : outbuf;
 
-   samps = chunksamps * inputchans;
+   int samps = framesToRun() * inputChannels();
 
-   if (cursamp < insamps)
+   if (currentFrame() < insamps)
       rtgetin(in, this, samps);
 
-   /* Scale input signal by amplitude multiplier and setline curve. */
-   for (int i = 0; i < samps; i += inputchans) {
-      if (cursamp < insamps) {           /* still taking input from file */
-         if (--branch < 0) {
+   // Scale input signal by amplitude multiplier and setline curve.
+   for (int i = 0; i < samps; i += inputChannels()) {
+      if (--branch <= 0) {
+         double p[11];
+         update(p, 11);
+         if (currentFrame() < insamps) {  // amp is pre-effect
+            amp = p[3];
             if (amparray)
-               aamp = tablei(cursamp, amparray, amptabs) * amp;
-            branch = skip;
+               amp *= tablei(currentFrame(), amparray, amptabs);
          }
-         in[i] *= aamp;
-         if (inputchans == 2)
-            in[i + 1] *= aamp;
+         updateRvb(p);
+         branch = skip;
       }
-      else {                             /* in ringdown phase */
+      if (currentFrame() < insamps) {     // still taking input from file
+         in[i] *= amp;
+         if (inputChannels() == 2)
+            in[i + 1] *= amp;
+      }
+      else {                              // in ringdown phase
          in[i] = 0.0;
-         if (inputchans == 2)
+         if (inputChannels() == 2)
             in[i + 1] = 0.0;
       }
-      cursamp++;
+      increment();
    }
 
-   /* Hand off to Freeverb to do the actual work. */
-   rvb->processreplace(inL, inR, outL, outR, chunksamps, inputchans,
-                                                         outputchans);
+   // Hand off to Freeverb to do the actual work.
+   rvb->processreplace(inL, inR, outL, outR, chunksamps, inputChannels(),
+                                                         outputChannels());
 
-   return chunksamps;
+   return framesToRun();
 }
 
 
--- insts/jg/FREEVERB/FREEVERB.h
+++ insts/jg/FREEVERB/FREEVERB.h
@@ -1,15 +1,20 @@
 #include "revmodel.hpp"
 
 class FREEVERB : public Instrument {
+   bool     warn_roomsize, warn_predelay, warn_damp, warn_dry, warn_wet,
+            warn_width;
    int      inchan, skip, branch, insamps;
-   float    amp, aamp, ringdur;
+   float    amp, ringdur, roomsize, predelay_time, max_roomsize,
+            damp, dry, wet, width;
    float    *in, *amparray, amptabs[2];
    revmodel *rvb;
 
+   void updateRvb(double p[]);
 public:
    FREEVERB();
    virtual ~FREEVERB();
-   int init(double p[], int n_args);
-   int run();
+   virtual int init(double p[], int n_args);
+   virtual int configure();
+   virtual int run();
 };
 
