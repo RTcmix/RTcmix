89ffd080562966578e13dbbc14a657267b1b197a
gibson <gibson>
2004-07-23 15:30:01 +0000

    PField updates.

--- insts/jg/EQ/EQ.cpp
+++ insts/jg/EQ/EQ.cpp
@@ -3,35 +3,58 @@
    p0 = output start time
    p1 = input start time
    p2 = input duration
-   p3 = amplitude multiplier
-   p4 = EQ type (0: low pass, 1: high pass, 2: low shelf, 3: high shelf,
-        4: peak/notch)
+   p3 = amplitude multiplier *
+   p4 = EQ type ("lowpass", "highpass", "lowshelf", "highshelf", "peaknotch")
    p5 = input channel [optional, default is 0]
    p6 = percent of signal to left output channel [optional, default is .5]
    p7 = bypass filter (0: no, 1: yes) [optional, default is 0]
+   p8 = filter frequency (Hz) [optional; if missing, must use gen 2] ***
+   p9 = filter Q (values from 0.5 to 10.0, roughly) [optional; if missing,
+        must use gen 3] ****
+   p10 = filter gain (dB) [shelf and peak/notch only; if missing, must
+         use gen 4] *****
 
-   Function tables:
-      1  amplitude curve (or use setline)
-         If no setline or function table 1, uses flat amplitude curve.
-      2  frequency (Hz)
-      3  Q (with values from 0.5 to 10.0, roughly)
-      4  gain (dB) [shelf and peak/notch only]
+   p3 (amplitude), p4 (type), p6 (pan), p7 (bypass), p8 (freq), p9 (Q)
+   and p10 (gain) can receive dynamic updates from a table or real-time
+   control source.
 
-   John Gibson <johgibso at indiana dot edu>, 7 Dec 2003
+   ----
+
+   Notes about backward compatibility with pre-v4 scores:
+
+   * If an old-style gen table 1 is present, its values will be multiplied
+   by p3 (amplitude), even if the latter is dynamic.
+
+   ** You can also give numeric codes for the EQ type (0: lowpass, 1: highpass,
+   2: lowshelf, 3: highshelf, 4: peaknotch).  These can be changed during a
+   note.  If you give the string version, you can't change types during a note.
+
+   *** If p8 is missing, you must use an old-style gen table 2 for the
+   filter frequency curve.
+
+   **** If p9 is missing, you must use an old-style gen table 3 for the
+   filter Q curve.
+
+   ***** If p10 is missing, you must use an old-style gen table 4 for the
+   filter gain curve.
+
+
+   John Gibson <johgibso at indiana dot edu>, 7 Dec 2003; rev for v4, 7/23/04
 
    Based on formulas by Robert Bristow-Johnson ("Audio-EQ-Cookbook") and code
    by Tom St Denis <tomstdenis.home.dhs.org> (see musicdsp.org)
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ugens.h>
 #include <math.h>
-#include <mixerr.h>
 #include <Instrument.h>
+#include <PField.h>
 #include "EQ.h"
 #include <rt.h>
 #include <rtdefs.h>
-#include <float.h>   /* for FLT_MIN, FLT_MAX */
+#include <float.h>   // for FLT_MIN, FLT_MAX
 
 
 EQ :: EQ() : Instrument()
@@ -42,7 +65,8 @@ EQ :: EQ() : Instrument()
    q_table = NULL;
    gain_table = NULL;
    branch = 0;
-   prev_freq = prev_Q = prev_gain = -FLT_MAX;
+   freq = Q = gain = -FLT_MAX;
+   eqtype_was_string = false;
 }
 
 
@@ -57,135 +81,208 @@ EQ :: ~EQ()
 }
 
 
+static char *_eqtype_name[] = {
+   "lowpass",     // 0
+   "highpass",    // 1
+   "lowshelf",    // 2
+   "highshelf",   // 3
+   "peaknotch",   // 4
+   NULL
+};
+
+inline int _string_to_eqcode(const char *str)
+{
+   for (int i = 0; _eqtype_name[i] != NULL; i++)
+      if (strncmp(str, _eqtype_name[i], 5) == 0)   // 5 is min. to distinguish
+         return i;
+   return -1;
+}
+
+EQType EQ :: getEQType(bool trystring)
+{
+   double index = (double) currentFrame() / nSamps();
+   const PField &field = getPField(4);
+
+   // must try int first, since a valid code cast to char * will crash strncmp
+   int code = field.intValue(index);
+   if ((code < 0 || code > 4) && trystring) {
+      const char *str = field.stringValue(index);
+      code = _string_to_eqcode(str);   // -1 if no match
+      if (code != -1)
+         eqtype_was_string = true;
+   }
+
+   EQType eqtype;
+   switch (code) {
+      case 0: eqtype = EQLowPass;   break;
+      case 1: eqtype = EQHighPass;  break;
+      case 2: eqtype = EQLowShelf;  break;
+      case 3: eqtype = EQHighShelf; break;
+      case 4: eqtype = EQPeaking;   break;
+      default: eqtype = EQInvalid;  break;
+   }
+   return eqtype;
+}
+
+
 int EQ :: init(double p[], int n_args)
 {
-   float outskip, inskip, dur;
-   int   type;
+   nargs = n_args;
    const float ringdur = 0.1;
-
-   outskip = p[0];
-   inskip = p[1];
-   dur = p[2];
+   float outskip = p[0];
+   float inskip = p[1];
+   float dur = p[2];
    amp = p[3];
-   type = (int) p[4];
-   inchan = n_args > 5 ? (int) p[5] : 0;           /* default is chan 0 */
-   pctleft = n_args > 6 ? p[6] : 0.5;              /* default is .5 */
-   bypass = n_args > 7 ? (int) p[7] : 0;           /* default is no */
+   inchan = n_args > 5 ? (int) p[5] : 0;           // default is chan 0
 
    if (rtsetinput(inskip, this) != 0)
       return DONT_SCHEDULE;
-   nsamps = rtsetoutput(outskip, dur + ringdur, this);
-   insamps = (int)(dur * SR + 0.5);
-
+   insamps = (int) (dur * SR + 0.5);
    if (inchan >= inputChannels())
       return die("EQ", "You asked for channel %d of a %d-channel file.",
                                                    inchan, inputChannels());
+   nsamps = rtsetoutput(outskip, dur + ringdur, this);
+   if (outputChannels() > 2)
+      return die("EQ", "Output must be mono or stereo.");
 
-   switch (type) {
-      case 0:
-         eqtype = EQLowPass;
-         break;
-      case 1:
-         eqtype = EQHighPass;
-         break;
-      case 2:
-         eqtype = EQLowShelf;
-         break;
-      case 3:
-         eqtype = EQHighShelf;
-         break;
-      case 4:
-         eqtype = EQPeaking;
-         break;
-   }
+   eqtype = getEQType(true);
+   if (eqtype == EQInvalid)
+      return die("EQ", "Type must be \"lowpass\", \"highpass\", \"lowshelf\", "
+                       "\"highshelf\" or \"peaknotch\".");
 
    double *function = floc(1);
    if (function) {
       int len = fsize(1);
       amp_table = new TableL(dur, function, len);
    }
-   else {
-      advise("EQ", "Setting amplitude curve to all 1's.");
-      aamp = amp;
-   }
 
-   function = floc(2);
-   if (function) {
+   if (n_args < 9) {       // no p8 filter freq PField, must use gen table
+      function = floc(2);
+      if (function == NULL)
+         return die("EQ", "Either use the EQ frequency pfield (p8) or make "
+                    "an old-style gen function in slot 2.");
       int len = fsize(2);
       freq_table = new TableL(dur, function, len);
    }
-   else
-      return die("EQ", "You haven't made the EQ frequency function (table 2).");
 
-   function = floc(3);
-   if (function) {
+   if (n_args < 10) {      // no p9 filter Q PField, must use gen table
+      function = floc(3);
+      if (function == NULL)
+         return die("EQ", "Either use the EQ Q pfield (p9) or make "
+                    "an old-style gen function in slot 3.");
       int len = fsize(3);
       q_table = new TableL(dur, function, len);
    }
-   else
-      return die("EQ", "You haven't made the EQ Q function (table 3).");
 
    if (eqtype != EQLowPass && eqtype != EQHighPass) {
-      function = floc(4);
-      if (function) {
+      if (n_args < 11) {      // no p10 filter gain PField, must use gen table
+         function = floc(4);
+         if (function == NULL)
+            return die("EQ", "Either use the EQ gain pfield (p10) or make "
+                       "an old-style gen function in slot 4.");
          int len = fsize(4);
          gain_table = new TableL(dur, function, len);
       }
-      else
-         return die("EQ", "You haven't made the EQ gain function (table 4).");
    }
-   else
-      gain = 0.0;
 
    eq = new Equalizer(eqtype);
 
    skip = (int) (SR / (float) resetval);
 
-   return nsamps;
+   return nSamps();
+}
+
+
+void EQ :: doupdate()
+{
+   double p[11];
+   update(p, 11, kAmp | kType | kPan | kBypass | kFreq | kQ | kGain);
+
+   amp = p[3];
+   if (amp_table)
+      amp *= amp_table->tick(currentFrame(), 1.0);
+
+   if (!eqtype_was_string) {
+      EQType type = getEQType(false);
+      if (type == EQInvalid)
+         type = EQLowPass;
+      if (type != eqtype) {
+         eqtype = type;
+         delete eq;
+         eq = new Equalizer(eqtype);
+         freq = -FLT_MAX;              // force setCoeffs call below
+      }
+   }
+
+   pctleft = nargs > 6 ? p[6] : 0.5;            // default is .5
+   bypass = nargs > 7 ? (bool) p[7] : false;    // default is no
+
+   float newfreq;
+   if (nargs > 8)
+      newfreq = p[8];
+   else
+      newfreq = freq_table->tick(currentFrame(), 1.0);
+   if (newfreq < 0.0)
+      newfreq = 0.0;
+   else if (newfreq > SR * 0.5)
+      newfreq = SR * 0.5;
+
+   float newQ;
+   if (nargs > 9)
+      newQ = p[9];
+   else
+      newQ = q_table->tick(currentFrame(), 1.0);
+   if (newQ <= 0.0)
+      newQ = FLT_MIN;
+
+   float newgain;
+   if (nargs > 10)
+      newgain = p[10];
+   else if (gain_table)
+      newgain = gain_table->tick(currentFrame(), 1.0);
+   else
+      newgain = 0.0;
+
+   if (newfreq != freq || newQ != Q || newgain != gain) {
+      freq = newfreq;
+      Q = newQ;
+      gain = newgain;
+      eq->setCoeffs(freq, Q, gain);
+   }
+}
+
+
+int EQ :: configure()
+{
+   in = new float [RTBUFSAMPS * inputChannels()];
+   return in ? 0 : -1;
 }
 
 
 int EQ :: run()
 {
-   int   i, samps;
-   float insig, outsig;
-   float out[2];
-
-   if (in == NULL)
-      in = new float [RTBUFSAMPS * inputChannels()];
-
-   samps = framesToRun() * inputChannels();
+   const int samps = framesToRun() * inputChannels();
    if (currentFrame() < insamps)
       rtgetin(in, this, samps);
 
-   for (i = 0; i < samps; i += inputChannels()) {
-      if (--branch < 0) {
-         if (amp_table)
-            aamp = amp_table->tick(currentFrame(), amp);
-         freq = freq_table->tick(currentFrame(), 1.0);
-         Q = q_table->tick(currentFrame(), 1.0);
-         if (Q <= 0.0)
-            Q = FLT_MIN;
-         if (gain_table)
-            gain = gain_table->tick(currentFrame(), 1.0);
-         if (freq != prev_freq || Q != prev_Q || gain != prev_gain)
-            eq->setCoeffs(freq, Q, gain);
-         prev_freq = freq;
-         prev_Q = Q;
-         prev_gain = gain;
+   for (int i = 0; i < samps; i += inputChannels()) {
+      if (--branch <= 0) {
+         doupdate();
          branch = skip;
       }
 
+      float insig = 0.0;
       if (currentFrame() < insamps)
          insig = in[i + inchan];
-      else
-         insig = 0.0;
 
+      float outsig;
       if (bypass)
          outsig = insig;
       else
-         outsig = (float) eq->tick((double) insig);
-      out[0] = outsig * aamp;
+         outsig = eq->tick(insig);
+
+      float out[2];
+      out[0] = outsig * amp;
 
       if (outputChannels() == 2) {
          out[1] = out[0] * (1.0 - pctleft);
--- insts/jg/EQ/EQ.h
+++ insts/jg/EQ/EQ.h
@@ -1,18 +1,32 @@
 #include <objlib.h>
 
 class EQ : public Instrument {
-   int         inchan, skip, branch, bypass, insamps;
-   float       amp, aamp, pctleft;
+   bool        bypass, eqtype_was_string;
+   int         nargs, inchan, skip, branch, insamps;
+   float       amp, pctleft, freq, Q, gain;
    float       *in;
-   float       freq, Q, gain, prev_freq, prev_Q, prev_gain;
    TableL      *amp_table, *freq_table, *q_table, *gain_table;
    Equalizer   *eq;
    EQType      eqtype;
 
+   EQType getEQType(bool trystring);
+   void doupdate();
 public:
    EQ();
    virtual ~EQ();
-   int init(double p[], int n_args);
-   int run();
+   virtual int init(double p[], int n_args);
+   virtual int configure();
+   virtual int run();
+};
+
+// update flags (shift amount is pfield index)
+enum {
+	kAmp = 1 << 3,
+	kType = 1 << 4,
+	kPan = 1 << 6,
+	kBypass = 1 << 7,
+	kFreq = 1 << 8,
+	kQ = 1 << 9,
+	kGain = 1 << 10
 };
 
