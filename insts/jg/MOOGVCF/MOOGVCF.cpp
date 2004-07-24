6d919c4d4ff3191da66e189a8ec17a48fe86cbe4
gibson <gibson>
2004-07-24 23:29:29 +0000

    PField and other updates.

--- insts/jg/MOOGVCF/MOOGVCF.cpp
+++ insts/jg/MOOGVCF/MOOGVCF.cpp
@@ -7,36 +7,51 @@
    p0 = output start time
    p1 = input start time
    p2 = input duration
-   p3 = amplitude multiplier
+   p3 = amplitude multiplier *
    p4 = input channel [optional, default is 0]
-   p5 = percent of signal to left output channel [optional, default is .5]
+   p5 = pan (in percent-to-left form: 0-1) [optional, default is .5]
+   p6 = bypass filter (0: no, 1: yes) [optional, default is 0]
+   p7 = filter cutoff frequency (Hz) [optional; if missing, must use gen 2] **
+   p8 = filter resonance (0-1) [optional; if missing, must use gen 3] ***
 
-   Assumes function table 1 is amplitude curve for the note. (Try gen 18.)
-   Or you can just call setline. If no setline or function table 1, uses
-   flat amplitude curve.
+   p3 (amplitude), p5 (pan), p6 (bypass), p7 (cutoff) and p8 (resonance) can
+   receive dynamic updates from a table or real-time control source.
 
-   Function table 2 is the cutoff frequency curve.
+   ----
 
-   Function table 3 is the resonance curve.  Should range between 0 and 1.
-   Easiest to use gen 18, and make sure values are in that range.
+   Notes about backward compatibility with pre-v4 scores:
 
-   John Gibson <johgibso at indiana dot edu>, 22 May 2002
+   * If an old-style gen table 1 is present, its values will be multiplied
+   by p3 (amplitude), even if the latter is dynamic.
+
+   ** If p7 is missing, you must use an old-style gen table 2 for the
+   filter cutoff frequency curve.
+
+   *** If p8 is missing, you must use an old-style gen table 3 for the
+   filter resonance curve.
+
+
+   John Gibson <johgibso at indiana dot edu>, 22 May 2002; rev for v4, 7/24/04
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ugens.h>
 #include <math.h>
-#include <mixerr.h>
 #include <Instrument.h>
 #include "MOOGVCF.h"
 #include <rt.h>
 #include <rtdefs.h>
+#include <float.h>   // for FLT_MAX
 
 
 MOOGVCF :: MOOGVCF() : Instrument()
 {
    in = NULL;
+   cfarray = NULL;
+   resarray = NULL;
    branch = 0;
+   cf = -FLT_MAX;
+   res = -FLT_MAX;
    b0 = b1 = b2 = b3 = b4 = 0.0;
 }
 
@@ -49,127 +64,146 @@ MOOGVCF :: ~MOOGVCF()
 
 int MOOGVCF :: init(double p[], int n_args)
 {
-   float outskip, inskip, dur, ringdown;
+   nargs = n_args;
+   float outskip = p[0];
+   float inskip = p[1];
+   float dur = p[2];
+   inchan = n_args > 4 ? (int) p[4] : 0;             // default is chan 0
 
-   outskip = p[0];
-   inskip = p[1];
-   dur = p[2];
-   amp = p[3];
-   inchan = n_args > 4 ? (int) p[4] : 0;             /* default is chan 0 */
-   pctleft = n_args > 5 ? p[5] : 0.5;                /* default is .5 */
-
-   ringdown = 0.2;      /* just a guess, for now */
+   float ringdown = 0.2;      // just a guess
    nsamps = rtsetoutput(outskip, dur + ringdown, this);
    if (rtsetinput(inskip, this) != 0)
       return DONT_SCHEDULE;
 
-   if (inchan >= inputchans)
+   if (inchan >= inputChannels())
       return die("MOOGVCF", "You asked for channel %d of a %d-channel file.",
-                                                         inchan, inputchans);
+                                                   inchan, inputChannels());
 
    amparray = floc(1);
    if (amparray) {
       int lenamp = fsize(1);
       tableset(SR, dur, lenamp, amptabs);
    }
-   else
-      advise("MOOGVCF", "Setting phrase curve to all 1's.");
 
-   cfarray = floc(2);
-   if (cfarray) {
-      int lencf = fsize(2);
-      tableset(SR, dur, lencf, cftabs);
-   }
-   else {
-      cf = 1000.0;
-      warn("MOOGVCF", "Without table 2, cutoff frequency set to 1kHz.");
+   if (n_args < 8) {       // no p7 cutoff freq PField, must use gen table
+      cfarray = floc(2);
+      if (cfarray == NULL)
+         return die("MOOGVCF", "Either use the cutoff frequency pfield (p7) "
+                    "or make an old-style gen function in slot 2.");
+      int len = fsize(2);
+      tableset(SR, dur, len, cftabs);
    }
 
-   resarray = floc(3);
-   if (resarray) {
-      int lenres = fsize(3);
-      tableset(SR, dur, lenres, restabs);
+   if (n_args < 9) {       // no p9 resonance PField, must use gen table
+      resarray = floc(3);
+      if (resarray == NULL)
+         return die("MOOGVCF", "Either use the resonance pfield (p8) "
+                    "or make an old-style gen function in slot 3.");
+      int len = fsize(3);
+      tableset(SR, dur, len, restabs);
    }
-   else {
-      res = 0.5;
-      warn("MOOGVCF", "Without table 3, resonance set to 0.5.");
-   }
-
-   make_coefficients();
 
    skip = (int) (SR / (float) resetval);
 
-   aamp = amp;                  /* in case amparray == NULL */
+   return nSamps();
+}
 
-   return nsamps;
+
+void MOOGVCF :: doupdate()
+{
+   double p[9];
+   update(p, 9, kAmp | kPan | kBypass | kCutoff | kResonance);
+
+   amp = p[3];
+   if (amparray)
+      amp *= tablei(currentFrame(), amparray, amptabs);
+
+   pctleft = nargs > 5 ? p[5] : 0.5;                  // default is .5
+   bypass = nargs > 6 ? (bool) p[6] : false;          // default is no
+
+   float newcf;
+   if (nargs > 7)
+      newcf = p[7];
+   else if (cfarray)
+      newcf = tablei(currentFrame(), cfarray, cftabs);
+   else
+      newcf = 1000.0;
+
+   float newres;
+   if (nargs > 8)
+      newres = p[8];
+   else if (resarray)
+      newres = tablei(currentFrame(), resarray, restabs);
+   else
+      newres = 0.5;
+
+   if (newcf != cf || newres != res) {
+      cf = newcf;
+      res = newres;
+      make_coefficients();
+   }
+}
+
+
+int MOOGVCF :: configure()
+{
+   in = new float [RTBUFSAMPS * inputChannels()];
+   return in ? 0 : -1;
 }
 
 
 int MOOGVCF :: run()
 {
-   int   i, samps;
-   float insig, t1, t2;
-   float out[2];
-
-   if (in == NULL)
-      in = new float [RTBUFSAMPS * inputchans];
-
-   samps = chunksamps * inputchans;
+   const int samps = framesToRun() * inputChannels();
    rtgetin(in, this, samps);
 
-   for (i = 0; i < samps; i += inputchans) {       /* loop for each frame */
-      if (--branch < 0) {
-         float newcf = cf, newres = res;
-         if (amparray)
-            aamp = tablei(cursamp, amparray, amptabs) * amp;
-         if (cfarray)
-            newcf = tablei(cursamp, cfarray, cftabs);
-         if (resarray)
-            newres = tablei(cursamp, resarray, restabs);
-         if (newcf != cf || newres != res) {
-            cf = newcf;
-            res = newres;
-            make_coefficients();
-         }
+   for (int i = 0; i < samps; i += inputChannels()) {
+      if (--branch <= 0) {
+         doupdate();
          branch = skip;
       }
 
-      insig = in[i + inchan] * aamp;
+      float insig = in[i + inchan] * amp;
 
-      /* Bring (normally) into range [-1, 1], because filter code
-         assumes this.
-      */
-      insig /= 32768.0;
+      float out[2];
 
-      insig -= q * b4;                             /* feedback */
+      if (bypass)
+         out[0] = insig;
+      else {
+         // Bring (normally) into range [-1, 1]; filter code assumes this.
+         insig *= (1.0 / 32768.0);
 
-      /* four cascaded onepole filters (bilinear transform) */
-      t1 = b1;  b1 = (insig + b0) * p - b1 * f;
-      t2 = b2;  b2 = (b1 + t1)    * p - b2 * f;
-      t1 = b3;  b3 = (b2 + t2)    * p - b3 * f;
-                b4 = (b3 + t1)    * p - b4 * f;
-      b4 = b4 - b4 * b4 * b4 * 0.166667;           /* clipping */
-      b0 = insig;
+         insig -= q * b4;           // feedback
 
-#if defined(HIGHPASS)      /* but only 6dB/oct */
-      out[0] = insig - b4;
-#elif defined(BANDPASS)    /* 6dB/oct */
-      out[0] = 3.0 * (b3 - b4);
+         // four cascaded onepole filters (bilinear transform)
+         float t1, t2;
+         t1 = b1;  b1 = (insig + b0) * p - b1 * f;
+         t2 = b2;  b2 = (b1 + t1)    * p - b2 * f;
+         t1 = b3;  b3 = (b2 + t2)    * p - b3 * f;
+                   b4 = (b3 + t1)    * p - b4 * f;
+         b4 = b4 - b4 * b4 * b4 * 0.166667;           // clipping
+         b0 = insig;
+
+#if defined(HIGHPASS)      // but only 6dB/oct
+         out[0] = insig - b4;
+#elif defined(BANDPASS)    // 6dB/oct
+         out[0] = 3.0 * (b3 - b4);
 #else
-      out[0] = b4;
+         out[0] = b4;
 #endif
-      out[0] *= 32768.0;
+         out[0] *= 32768.0;
+      }
 
-      if (outputchans == 2) {
+      if (outputChannels() == 2) {
          out[1] = out[0] * (1.0 - pctleft);
          out[0] *= pctleft;
       }
 
       rtaddout(out);
-      cursamp++;
+      increment();
    }
 
-   return chunksamps;
+   return framesToRun();
 }
 
 
@@ -183,9 +217,9 @@ Instrument *makeMOOGVCF()
    return inst;
 }
 
+
 void rtprofile()
 {
    RT_INTRO("MOOGVCF", makeMOOGVCF);
 }
-
 
--- insts/jg/MOOGVCF/MOOGVCF1.sco
+++ insts/jg/MOOGVCF/MOOGVCF1.sco
@@ -1,27 +1,3 @@
-/* MOOGVCF - a 24dB/octave resonant lowpass filter
-
-   This is based on the design by Stilson and Smith (CCRMA), as modified
-   by Paul Kellett <paul.kellett@maxim.abel.co.uk> (described in the source
-   code archives of the Music DSP site -- musicdsp.org).
-
-   p0 = output start time
-   p1 = input start time
-   p2 = input duration
-   p3 = amplitude multiplier
-   p4 = input channel [optional, default is 0]
-   p5 = percent of signal to left output channel [optional, default is .5]
-
-   Assumes function table 1 is amplitude curve for the note. (Try gen 18.)
-   Or you can just call setline. If no setline or function table 1, uses
-   flat amplitude curve.
-
-   Function table 2 is the cutoff frequency curve.
-
-   Function table 3 is the resonance curve.  Should range between 0 and 1.
-   Easiest to use gen 18, and make sure values are in that range.
-
-   John Gibson <johgibso at indiana dot edu>, 22 May 2002
-*/
 rtsetparams(44100, 1)
 load("WAVETABLE")
 load("MOOGVCF")
