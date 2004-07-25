3fb9e3a841f15e5c84a860f5a3836fce628adfea
gibson <gibson>
2004-07-25 15:14:25 +0000

    Update to new bookkeeping array format.

--- genlib/delput.c
+++ genlib/delput.c
@@ -1,9 +1,10 @@
+/* put value in delay line. See delset. x is float */
 void
 delput(float x, float *a, int *l)
 {
-
-/* put value in delay line. See delset. x is float */
-
-	*(a + (*l)++) = x;
-	if(*(l) >= *(l+1)) *l -= *(l+1);
+	int index = l[0];
+	a[index] = x;
+	l[0]++;
+	if (l[0] >= l[2])
+		l[0] -= l[2];
 }
