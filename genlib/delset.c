b82b614b4361f85fa1ec70fe4c94e249eef11da4
gibson <gibson>
2004-07-25 15:14:19 +0000

    Clarify comment.

--- genlib/delset.c
+++ genlib/delset.c
@@ -1,13 +1,12 @@
-
+/* delay initialization.  a is address of float array, l is array of 3 ints
+   for bookkeeping variables, xmax, is maximum expected delay
+*/
 void
 delset(float SR, float *a, int *l, float xmax)
 {
-/* delay initialization.  a is address of float array, l is size+3 int 
- * array for bookkeeping variables, xmax, is maximum expected delay */
-
 	int i;
 	l[0] = 0;
 	l[1] = SR;
 	l[2] = (int)(xmax * SR + .5);
-	for(i = 0; i < l[2]; i++) a[i] = 0;
+	for (i = 0; i < l[2]; i++) a[i] = 0;
 }
