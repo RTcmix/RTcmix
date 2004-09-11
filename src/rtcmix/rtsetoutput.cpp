8701b6e68ce876ad7a7c7c09f5b56d887cb10ccc
gibson <gibson>
2004-09-11 22:20:08 +0000

    Fix roundoff error setting nsamps; return status rather than nsamps.

--- src/rtcmix/rtsetoutput.cpp
+++ src/rtcmix/rtsetoutput.cpp
@@ -10,23 +10,18 @@
 #include "dbug.h"
 #include <stdlib.h>
 
-//extern double schedtime;
-
 int Instrument::rtsetoutput(float start, float dur, Instrument *theInst)
 {
-// I know this is silly, but I wanted nsamps and I wanted it to
-// look like "setnote" in orig cmix insts
-  // DJT:  then perhaps we should call it rtsetnote?
+	if (rtfileit < 0) {
+		die(theInst->name(),
+					"No output file open for this instrument (rtoutput failed?)!");
+		return -1;
+	}
 
-  // DS: Adding check to be sure rtoutput() did not fail.
+	theInst->_start = start;
+	theInst->_dur = dur;
+	theInst->nsamps = (int) (dur * SR + 0.5);
 
-  if (rtfileit < 0) {
-  	 die(theInst->name(), "rtsetoutput: No output file open for this instrument (rtoutput failed?)!");
-  }
-  
-  theInst->_start = start;
-  theInst->_dur = dur;
-  theInst->nsamps = (int)(dur * SR);
-  
-  return(theInst->nsamps);
+	return 0;
 }
+
