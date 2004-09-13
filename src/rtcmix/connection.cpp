d79bd7b3ceab35bf23f3e50ce46b8d4c2ba4dbac
gibson <gibson>
2004-09-13 21:12:21 +0000

    Add midi stuff.

--- src/rtcmix/connection.cpp
+++ src/rtcmix/connection.cpp
@@ -15,6 +15,7 @@
 #include <ugens.h>		// for warn, die
 
 extern RTNumberPField *mouse_connection(const Arg args[], const int nargs);
+extern RTNumberPField *midi_connection(const Arg args[], const int nargs);
 
 
 // --------------------------------------------------------- local utilities ---
@@ -40,25 +41,27 @@ extern "C" {
 Handle
 makeconnection(const Arg args[], const int nargs)
 {
-#ifdef MACOSX
-	die("makeconnection", "Not yet implemented for OS X.");
-	return NULL;
-#else
 	if (!args[0].isType(StringType)) {
 		die("makeconnection", "First argument must be a string giving "
 									"connection type, e.g. \"mouseX\", \"midi\".");
 		return NULL;
 	}
 
-	RTNumberPField *connection;
+	RTNumberPField *connection = NULL;
 
 	if (args[0] == "mouseX" || args[0] == "mouseY")
+#ifdef MACOSX
+{
+		die("makeconnection (mouse)", "Not yet implemented for OS X.");
+		return NULL;
+}
+#else
 		connection = mouse_connection(args, nargs);
+#endif
 
 	if (connection == NULL)
 		return NULL;
 
 	return _createPFieldHandle(connection);
-#endif
 }
 
