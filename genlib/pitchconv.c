daa0da80b1537fc6fef11d1683a8b16466b529f2
gibson <gibson>
2004-11-25 18:32:21 +0000

    Enable inlining

--- genlib/pitchconv.c
+++ genlib/pitchconv.c
@@ -6,53 +6,62 @@
 #include <math.h>
 #include <ugens.h>
 
+#ifdef __GNUC__
+#define INLINE inline
+#else
+#define INLINE
+#endif
+
 // midC freq is 440.0 * pow(2.0, -9.0 / 12.0) = 261.625...
 #define MIDC_OFFSET (261.62556530059868 / 256.0)
-#define LOG_OF_2    0.69314718055994529
 
-double cpsoct(double oct)
+#ifndef M_LN2		// log_e 2
+	#define M_LN2	0.69314718055994529
+#endif
+
+INLINE double cpsoct(double oct)
 {
 	return pow(2.0, oct) * MIDC_OFFSET;
 }
 
-double octcps(double cps)
+INLINE double octcps(double cps)
 {
-	return log(cps / MIDC_OFFSET) / LOG_OF_2;
+	return log(cps / MIDC_OFFSET) / M_LN2;
 }
 
-double cpspch(double pch)
-{
-	double oct = octpch(pch);
-	return pow(2.0, oct) * MIDC_OFFSET;
-}
-
-double pchcps(double cps)
-{
-	double oct = octcps(cps);
-	return pchoct(oct);
-}
-
-double octpch(double pch)
+INLINE double octpch(double pch)
 {
 	int octave = (int) pch;
 	double semitone = (100.0 / 12.0) * (pch - octave);
 	return octave + semitone;
 }
 
-double pchoct(double oct)
+INLINE double cpspch(double pch)
+{
+	double oct = octpch(pch);
+	return cpsoct(oct);
+}
+
+INLINE double pchoct(double oct)
 {
 	int octave = (int) oct;
 	double linsemitone = oct - octave;
 	return octave + (0.12 * linsemitone); 
 }
 
-double midipch(double pch)
+INLINE double pchcps(double cps)
+{
+	double oct = octcps(cps);
+	return pchoct(oct);
+}
+
+INLINE double midipch(double pch)
 {
 	int midinote = (int) (((octpch(pch) - 3.0) * 12.0) + 0.5);
 	return midinote;
 }
 
-double pchmidi(unsigned char midinote)
+INLINE double pchmidi(unsigned char midinote)
 {
 	return pchoct(((double) midinote / 12.0) + 3.0);
 }
