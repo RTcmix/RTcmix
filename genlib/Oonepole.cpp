de078313f7f132c4ba7a7ea6d6e48c29d7742f90
gibson <gibson>
2004-11-30 20:31:06 +0000

    Subclass to track lag/freq changes.

--- genlib/Oonepole.cpp
+++ genlib/Oonepole.cpp
@@ -5,6 +5,18 @@
 // Adapted from Dodge and Jerse, and STK OnePole, by JGG
 
 #include <Ougens.h>
+#include <float.h>	// for -FLT_MAX
+
+//#define DEBUG
+
+#ifdef DEBUG
+  #include <stdio.h>
+  #define DPRINT(msg)			printf((msg))
+  #define DPRINT1(msg, arg)	printf((msg), (arg))
+#else
+  #define DPRINT(msg)
+  #define DPRINT1(msg, arg)
+#endif
 
 Oonepole::Oonepole(float SR) : _sr(SR), _hist(0.0), _a(0.1), _b(0.9)
 {
@@ -19,6 +31,8 @@ Oonepole::Oonepole(float SR, float freq) : _sr(SR), _hist(0.0)
 
 void Oonepole::setfreq(float freq)
 {
+	DPRINT1("Oonepole::setfreq(%f)\n", freq);
+
 	if (freq >= 0.0) {
 		double c = 2.0 - cos(freq * (M_PI * 2.0) / _sr);
 		_b = -(sqrt(c * c - 1.0) - c);
@@ -39,9 +53,40 @@ void Oonepole::setfreq(float freq)
 
 void Oonepole::setlag(float lag)
 {
+	DPRINT1("Oonepole::setlag(%f)\n", lag);
+
 	double cf = MAXCF * pow(2, -lag * LAGFACTOR);
 	if (cf > _sr * 0.5)		// if control rate < 1000
 		cf = _sr * 0.5;
 	setfreq(cf);
+}
+
+
+// ----------------------------------------------------------- OonepoleTrack ---
+
+OonepoleTrack::OonepoleTrack(float SR) : Oonepole(SR)
+{
+	_freq = -FLT_MAX;
+	_lag = -FLT_MAX;
+}
+
+void OonepoleTrack::setfreq(float freq)
+{
+	DPRINT1("OonepoleTrack::setfreq(%f)\n", freq);
+
+	if (freq != _freq) {
+		Oonepole::setfreq(freq);
+		_freq = freq;
+	}
+}
+
+void OonepoleTrack::setlag(float lag)
+{
+	DPRINT1("OonepoleTrack::setlag(%f)\n", lag);
+
+	if (lag != _lag) {
+		Oonepole::setlag(lag);
+		_lag = lag;
+	}
 }
 
