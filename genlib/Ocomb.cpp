0b8b31f9c9a1b90ef11fc5a0055fbc4713e5632e
dscott <dscott>
2005-01-11 01:41:31 +0000

    Testing a new resizable delay unit

--- genlib/Ocomb.cpp
+++ genlib/Ocomb.cpp
@@ -5,6 +5,30 @@
 #include <Ocomb.h>
 #include <math.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static float *newFloats(float *oldptr, int oldlen, int *newlen)
+{
+	float *ptr = NULL;
+	if (oldptr == NULL) {
+		ptr = (float *) malloc(*newlen * sizeof(float));
+	}
+	else {
+		float *newptr = (float *) realloc(oldptr, *newlen * sizeof(float));
+		if (newptr) {
+			ptr = newptr;
+			// Zero out new portion.
+			for (int n = oldlen; n < *newlen; ++n)
+				ptr[n] = 0.0f;
+		}
+		else {
+			*newlen = oldlen;	// notify caller that realloc failed
+			ptr = oldptr;
+		}
+	}
+	return ptr;
+}
 
 Ocomb::Ocomb(float SR, float loopTime, float reverbTime) : _sr(SR)
 {
@@ -23,7 +47,7 @@ void Ocomb::init(float loopTime, float maxLoopTime, float reverbTime)
 	assert(maxLoopTime >= loopTime);
 
 	_len = (int) (maxLoopTime * _sr + 0.5);
-	_dline = new float[_len];
+	_dline = newFloats(NULL, 0, &_len);
 	clear();
 	_delsamps = (int) (loopTime * _sr + 0.5);
 	setReverbTime(reverbTime);
@@ -32,7 +56,7 @@ void Ocomb::init(float loopTime, float maxLoopTime, float reverbTime)
 
 Ocomb::~Ocomb()
 {
-	delete [] _dline;
+	free(_dline);
 }
 
 void Ocomb::clear()
@@ -58,6 +82,19 @@ float Ocomb::next(float input)
 
 float Ocomb::next(float input, int delaySamps)
 {
+	if (delaySamps >= _len) {
+		// Make a guess at how big the new array should be.
+		int newlen = (delaySamps < _len * 2) ? _len * 2 : _len + delaySamps;
+		printf("Ocomb resizing from %d to %d\n", _len, newlen);
+		_dline = newFloats(_dline, _len, &newlen);
+		if (newlen > _len) {
+			_len = newlen;
+		}
+		else {
+			printf("Ocomb resize failed!  Limiting delay.\n");
+			delaySamps = _len - 1;
+		}
+	}
 	_delsamps = delaySamps;
 	if (_pointer >= _delsamps)
 		_pointer = 0;
