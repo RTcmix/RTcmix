046186d31eebc61365fd1e481faf53fa1f2f88f7
dscott <dscott>
2005-01-25 22:11:11 +0000

    Added comments

--- insts/std/MMOVE/moveprof.c
+++ insts/std/MMOVE/moveprof.c
@@ -3,21 +3,31 @@
 int
 profile()
 {
-	UG_INTRO("oldmatrix",oldmatrix); 
-	UG_INTRO("matrix",matrix); 
-	UG_INTRO("space",space); 
-	UG_INTRO("mikes",mikes); 
-	UG_INTRO("mikes_off",mikes_off); 
+	UG_INTRO("oldmatrix",m_oldmatrix); 
+	UG_INTRO("matrix",m_matrix); 
+	UG_INTRO("space",m_space); 
+	UG_INTRO("mikes",m_mikes); 
+	UG_INTRO("mikes_off",m_mikes_off); 
 	UG_INTRO("param",param); 
 	UG_INTRO("cparam",cparam); 
 	UG_INTRO("path",path); 
 	UG_INTRO("cpath",cpath); 
   	UG_INTRO("threshold",threshold); 
-  	UG_INTRO("set_attenuation_params",set_attenuation_params); 
+  	UG_INTRO("set_attenuation_params",m_set_attenuation_params); 
   	return 0;
 }
 
 static const char *dsoName = "libMMOVE";
+
+// This function is called, if present, by RTcmix at initialization time
+// when it locates and opens the DSO containing this function.  The call
+// to registerFunction() takes the string tag for a function that can be
+// called from a Minc script, and the name of the DSO containing the function.
+// These should all be the same for any given instrument.
+//
+// At the present time, there is no system to handle multiple attempts to
+// register the same function tag.  Only the first attempt will succeed.
+//
 
 int
 registerSelf()
