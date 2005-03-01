3dc3fdd0b997f2c565da1de0135587ea563f83b1
gibson <gibson>
2005-03-01 20:02:52 +0000

    more chars

--- src/control/display/labels.h
+++ src/control/display/labels.h
@@ -1,13 +1,13 @@
 #ifndef _LABELS_H_
 #define _LABELS_H_
 
-#define NLABELS				12		// Max. number of labels
+#define NLABELS				12    // Max. number of labels
 
-#define WHOLE_LABEL_LENGTH	64		// Total number of chars in whole label
-#define PART_LABEL_LENGTH	16		// Total number of chars in label component,
-											// i.e., prefix and units strings
+#define WHOLE_LABEL_LENGTH	128   // Total number of chars in whole label
+#define PART_LABEL_LENGTH	32    // Total number of chars in label component
+                                 // (i.e., prefix and units strings)
 
-#define LABEL_FROM_LEFT		10		// pixels from left border of window
+#define LABEL_FROM_LEFT		10   // pixels from left border of window
 #define LABEL_FROM_TOP		10
 
 #endif // _LABELS_H_
