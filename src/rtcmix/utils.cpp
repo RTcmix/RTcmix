f35dfa2aa4c6a7752d777dbc7131c5d5e0cd443e
dscott <dscott>
2005-03-17 21:27:07 +0000

    First pass on completion of handle utils.

--- src/rtcmix/utils.cpp
+++ src/rtcmix/utils.cpp
@@ -6,15 +6,59 @@
 //
 
 #include <Handle.h>
+#include <PField.h>
 #include <stdlib.h>
+#include <assert.h>
 
 Handle
 createPFieldHandle(PField *pfield)
 {
 	Handle handle = (Handle) malloc(sizeof(struct _handle));
-	handle->type = PFieldType;
-	handle->ptr = (void *) pfield;
-	handle->refcount = 0;
+	if (handle) {
+		handle->type = PFieldType;
+		handle->ptr = (void *) pfield;
+#ifdef DEBUG
+    	printf("\trefing PField %p\n", pfield);
+#endif
+		pfield->ref();
+		handle->refcount = 0;
+	}
 	return handle;
 }
+
+Handle
+createInstHandle(Instrument *inst)
+{
+	Handle handle = (Handle) malloc(sizeof(struct _handle));
+	if (handle) {
+		handle->type = InstrumentPtrType;
+		handle->ptr = (void *) inst;
+		handle->refcount = 0;
+	}
+	return handle;
+}
+
+void
+unrefHandle(Handle h)
+{
+	assert(h->refcount >= 0);
+#ifdef DEBUG
+    printf("unrefHandle(%p): %d -> ", h, h->refcount);
+#endif
+	--h->refcount;
+#ifdef DEBUG
+    printf("%d\n", h->refcount);
+#endif
+    if (h->refcount == 0 && h->type == PFieldType)
+    {
+#ifdef DEBUG
+        printf("\tfreeing Handle %p\n", h);
+		h->refcount = 0xdeaddead;	// for debugging
+        printf("\tunrefing PField %p\n", h->ptr);
+#endif
+		RefCounted::unref((PField *)h->ptr);
+        free(h);
+    }
+}	
+
 
--- src/rtcmix/utils.h
+++ src/rtcmix/utils.h
@@ -4,5 +4,39 @@
 */
 
 #include <rtcmix_types.h>
+#include <assert.h>
+#include <stdio.h>
 
-Handle createPFieldHandle(PField *);
+#ifdef __cplusplus
+Handle createPFieldHandle(class PField *);
+Handle createInstHandle(class Instrument *);
+inline void refHandle(Handle h)
+{
+	assert(h->refcount >= 0);
+    printf("refHandle(%p): %d -> ", h, h->refcount);
+	++h->refcount;
+    printf("%d\n", h->refcount);
+}
+#endif	// __cplusplus
+
+#ifdef __cplusplus
+extern "C" {
+#endif	// __cplusplus
+
+void unrefHandle(Handle);
+
+#ifdef __cplusplus
+}
+#endif	// __cplusplus
+
+#ifndef __cplusplus
+// This is macro-ized to allow inlining in C
+#define refHandle(h) \
+	{ \
+	printf("refHandle(%p): %d -> ", h, h->refcount); \
+	assert(h->refcount >= 0); \
+	++h->refcount; \
+	printf("%d\n", h->refcount); \
+	} 
+
+#endif	// !__cplusplus
