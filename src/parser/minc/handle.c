44b35de5471ad697ebc35db9fb00c7eff4c5701b
dscott <dscott>
2005-03-18 01:34:14 +0000

    New files to hold opaque handle manipulators

---
+++ src/parser/minc/handle.c
@@ -0,0 +1,19 @@
+// handle.c 
+// Special interface code to allow reference counting on opaque handles
+
+#include <minc_internal.h>
+#include "../../rtcmix/utils.h"
+
+void
+ref_handle(MincHandle h)
+{
+	Handle handle = (Handle) h;
+	refHandle(handle);
+}
+
+void
+unref_handle(MincHandle h)
+{
+	Handle handle = (Handle) h;
+	unrefHandle(handle);
+}
