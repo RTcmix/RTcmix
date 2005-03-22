d82fb2be455b1edc323d4c3133d1802f28768548
dscott <dscott>
2005-03-22 22:49:27 +0000

    Inst handles now ref and unref correctly.

--- src/rtcmix/utils.cpp
+++ src/rtcmix/utils.cpp
@@ -19,7 +19,7 @@ createPFieldHandle(PField *pfield)
 		handle->type = PFieldType;
 		handle->ptr = (void *) pfield;
 #ifdef DEBUG
-		printf("\tcreated Handle %p\n", handle);
+		printf("\tcreated PField Handle %p\n", handle);
     	printf("\t\trefing PField %p\n", pfield);
 #endif
 		pfield->ref();
@@ -35,6 +35,10 @@ createInstHandle(Instrument *inst)
 	if (handle) {
 		handle->type = InstrumentPtrType;
 		handle->ptr = (void *) inst;
+#ifdef DEBUG
+		printf("\tcreated Instrument Handle %p\n", handle);
+    	printf("\t\t (not refing) inst = %p\n", inst);
+#endif
 		handle->refcount = 0;
 	}
 	return handle;
@@ -51,14 +55,27 @@ unrefHandle(Handle h)
 #ifdef DEBUG
     printf("%d\n", h->refcount);
 #endif
-    if (h->refcount == 0 && h->type == PFieldType)
+    if (h->refcount == 0)
     {
 #ifdef DEBUG
         printf("\tfreeing Handle %p\n", h);
 		h->refcount = 0xdeaddead;	// for debugging
-        printf("\tunrefing PField %p\n", h->ptr);
 #endif
-		RefCounted::unref((PField *)h->ptr);
+		if (h->type == PFieldType) {
+#ifdef DEBUG
+			printf("\tunrefing PField %p\n", h->ptr);
+#endif
+			RefCounted::unref((RefCounted *)h->ptr);
+		}
+		else if (h->type == InstrumentPtrType) {
+#ifdef DEBUG
+			printf("\tnot yet unrefing inst %p\n", h->ptr);
+#endif
+		}
+		else {
+			fprintf(stderr, "unrefHandle: unknown handle type!\n");
+			return;
+		}
         free(h);
     }
 }	
