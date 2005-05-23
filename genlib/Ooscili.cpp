ff9ecb4f5039bc186a75f5427c9be7834c3ca70c
gibson <gibson>
2005-05-23 15:30:59 +0000

    Fixed-point phase calculations.

--- genlib/Ooscili.cpp
+++ genlib/Ooscili.cpp
@@ -4,6 +4,12 @@
 */
 #include <Ooscili.h>
 #include <ugens.h>
+//#define NDEBUG
+#include <assert.h>
+
+#define kFracBits 16
+#define kFracShift 65536
+#define kFracMask kFracShift - 1
 
 Ooscili::Ooscili(float SR, float freq, int arr) : _sr(SR)
 {
@@ -28,37 +34,43 @@ Ooscili::Ooscili(float SR, float freq, double arr[], int len) : _sr(SR)
 
 void Ooscili::init(float freq)
 {
+	assert(length < kFracShift / 2);
 	lendivSR = (double) length / _sr;
-	si = freq * lendivSR;
-	phase = 0.0;
-	dur = 1.0 / freq;	// for arbitrary lookups in the next(nsample) method
+	si = fp(freq * lendivSR);
+	phase = 0;
+
+	// for arbitrary lookups in the next(nsample) method
+	tabscale = (double) (length - 1) / (_sr / freq);
 }
 
 float Ooscili::next()
 {
-	int i = (int) phase;
-	int k = (i+1) % length;
-	double frac = phase - (double) i;
-	float output = array[i] + ((array[k] - array[i]) * frac);
+	int i = phase >> kFracBits;
+	int k = i + 1;
+	if (k >= length)
+		k = 0;
+	const int frac = phase & kFracMask;
+	float output = array[i] + (((array[k] - array[i]) * frac) / kFracShift);
 
 	// prepare for next call
 	phase += si;
-	while (phase >= (double) length)
-		phase -= (double) length;
-	while (phase < 0.0)
-		phase += (double) length;
+	fixed_t fplen = length << kFracBits;
+	while (phase >= fplen)
+		phase -= fplen;
+	while (phase < 0)		// handle negative freqs
+		phase += fplen;
 
 	return output;
 }
 
 float Ooscili::next(int nsample)
 {
-	double frac = ((double)nsample/_sr)/dur * (double)(length-1);
-	if (frac >= (double)(length-1))
-		return array[length-1];
-	int loc1 = (int) frac;
-	int loc2 = loc1 + 1;
-	frac = frac - (double) loc1;
-	return array[loc1] + (frac * (array[loc2] - array[loc1]));
+	assert(nsample >= 0);
+	double frac = nsample * tabscale;
+	int i = (int) frac;
+	if (i >= length - 1)			// NB: we read array[i + 1]
+		return array[length - 1];
+	frac = frac - (double) i;
+	return array[i] + (frac * (array[i + 1] - array[i]));
 }
 
--- genlib/Ooscili.h
+++ genlib/Ooscili.h
@@ -5,12 +5,25 @@
 #ifndef _OOSCILI_H_
 #define _OOSCILI_H_ 1
 
+#include <stdint.h>
+
+// We use a 16.16 fixed-point integer for phase calculations.  65536 (2^16)
+// is the amount to shift each half of the integer (whole and decimal).
+// NOTE: we could double the size of this on 64-bit platforms, but be
+// careful of overflow when multiplying by <frac> in next().   If changing,
+// note macros at top of .cpp file.
+
+typedef int32_t fixed_t;	// 16.16 fixed point
+
+inline fixed_t fp(double val) { return (fixed_t) (val * 65536); }
+
 class Ooscili
 {
-	double si, phase, lendivSR;
+	double lendivSR;
+	double tabscale;
+	fixed_t si, phase;
 	double *array;
 	float _sr;
-	float dur;
 	int length;
 
 	void init(float);
@@ -20,10 +33,10 @@ public:
 	Ooscili(float SR, float freq, double arr[], int len);
 	float next();
 	float next(int nsample);
-	inline void setfreq(float freq) { si = freq * lendivSR; }
-	inline void setphase(double phs) { phase = phs; }
-	inline int getlength() { return length; }
-	inline float getdur() { return dur; }
+	inline void setfreq(float freq) { si = fp(freq * lendivSR); }
+	inline void setphase(double phs) { phase = fp(phs); }
+	inline int getlength() const { return length; }
+//	inline float getdur() const { return 1.0 / freq; }
 };
 
 #endif // _OOSCILI_H_
