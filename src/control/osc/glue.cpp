bded19b430ff425993c72ff44d8e744561beba1e
jgibson <jgibson>
2005-11-30 16:03:36 +0000

    New arg order.

--- src/control/osc/glue.cpp
+++ src/control/osc/glue.cpp
@@ -18,32 +18,35 @@
 
 // -----------------------------------------------------------------------------
 //
-//   osc = makeconnection("osc", path, index, min, max, default, lag
-//                          [, input_min, input_max])
+//   osc = makeconnection("osc", path, index, inmin, inmax, outmin, outmax,
+//                        default, lag)
 //
 //   <path>      OSC path string to match (e.g., "/synth/oscil2/frequency")
 //
-//   <index>     zero-based index of item in message to read
+//   <index>     zero-based index of item in message to read.  (The item will
+//               be coerced to a double, unless it's not a number, in which case
+//               it won't be read.)
 //
-//   <min, max>  range of numbers to output
+//   <inmin,     range of raw data to expect
+//    inmax>
+//
+//   <outmin,    range of numbers to output
+//    outmax>
 //
 //   <default>   number to output before first matching OSC message received
 //
 //   <lag>       amount of smoothing of output signal (0-100)
 //
-//   <input_min, range of raw data to expect.  If missing, expect [0-1].
-//   input_max>
-//
 //
 //   Example:
 //
-//   osc = makeconnection("osc", "/synth/freqs", index=1, min=100,
-//                         max=1000, default=100, lag=40)
+//   osc = makeconnection("osc", "/synth/freqs", index=1, inmin=-1, inmax=1,
+//                         outmin=100, outmax=1000, default=100, lag=40)
 //
 //   If the OSC server receives messages with a path of "/synth/freqs",
-//   this PField returns the second item as a float, scaled to fit between
-//   100 and 1000 inclusive.  Incoming OSC values are assumed to fall between
-//   0 and 1.  The PField returns 100 if accessed prior to receiving any
+//   this PField returns the second item as a double, scaled to fit between
+//   100 and 1000 inclusive.  Incoming OSC values are expected to fall between
+//   -1 and 1.  The PField returns 100 if accessed prior to receiving any
 //   matching OSC messages.  The stream of values is smoothed using a lag
 //   value of 40.
 
@@ -51,48 +54,62 @@ static RTNumberPField *
 _osc_usage()
 {
 	die("makeconnection (osc)",
-		"Usage: makeconnection(\"osc\", path, index, min, max, default, "
-		"lag[, input_min, input_max])");
+		"Usage: makeconnection(\"osc\", path, index, inmin, inmax, "
+		                       "outmin, outmax, default, lag)");
 	return NULL;
 }
 
 static RTNumberPField *
 create_pfield(const Arg args[], const int nargs)
 {
-	if (nargs < 6)
+	if (nargs < 8)
 		return _osc_usage();
 
-	double outputmin, outputmax, defaultval, lag, inputmin = 0.0, inputmax = 1.0;
-	int index;
 	const char *path;
-
 	if (args[0].isType(StringType))
 		path = args[0];
 	else
 		return _osc_usage();
 
+	int index;
 	if (args[1].isType(DoubleType))
 		index = (int) args[1];
 	else
 		return _osc_usage();
+	if (index < 0) {
+		die("makeconnection (osc)", "<index> must be 0 or greater");
+		return NULL;
+	}
+
+	double inmin, inmax, outmin, outmax, defaultval, lag;
 
 	if (args[2].isType(DoubleType))
-		outputmin = args[2];
+		inmin = args[2];
 	else
 		return _osc_usage();
 
 	if (args[3].isType(DoubleType))
-		outputmax = args[3];
+		inmax = args[3];
 	else
 		return _osc_usage();
 
 	if (args[4].isType(DoubleType))
-		defaultval = args[4];
+		outmin = args[4];
 	else
 		return _osc_usage();
 
 	if (args[5].isType(DoubleType))
-		lag = args[5];
+		outmax = args[5];
+	else
+		return _osc_usage();
+
+	if (args[6].isType(DoubleType))
+		defaultval = args[6];
+	else
+		return _osc_usage();
+
+	if (args[7].isType(DoubleType))
+		lag = args[7];
 	else
 		return _osc_usage();
 	if (lag < 0.0 || lag > 100.0) {
@@ -101,29 +118,14 @@ create_pfield(const Arg args[], const int nargs)
 	}
 	lag *= 0.01;
 
-	if (nargs > 6) {
-		if (args[6].isType(DoubleType))
-			inputmin = args[6];
-		else
-			return _osc_usage();
-		if (nargs > 7) {
-			if (args[7].isType(DoubleType))
-				inputmax = args[7];
-			else
-				return _osc_usage();
-		}
-		else
-			return _osc_usage();
-	}
-
 	static RTcmixOSC *oscserver = NULL;
 	if (oscserver == NULL)				// first time, so init OSC server
 		oscserver = createOSCServer();
 	if (oscserver == NULL)
 		return NULL;
 
-	return new RTOscPField(oscserver, path, index, inputmin, inputmax,
-	                           outputmin, outputmax, defaultval, lag);
+	return new RTOscPField(oscserver, path, index, inmin, inmax, outmin, outmax,
+	                       defaultval, lag);
 }
 
 
