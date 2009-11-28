42e7ca88db820f27fe0c33c41d380371711f0bbd
bgarton <bgarton>
2009-11-28 01:09:02 +0000

    the PFSCHED instrument for allowing dynamic and scheduled PFields -- bgg, 11/27/2009

---
+++ insts/bgg/PFSCHED/PFSCHED.cpp
@@ -0,0 +1,97 @@
+/* PFSCHED - 'vehicle' instrument to allow schedule PField values
+
+	PFSCHED takes a PField as an input and will read from it
+	at a scheduled time for a scheduled duration.  These values
+	are then written to a 'pfield bus' specified in a makeconnection()
+	scorefile cammand.  The PField handle from that makeconnection()
+	can then be used to feed scheduled PField values into an
+	executing instrument
+
+	p0 = output start time
+	p1 = duration
+	p2 = 'pfield bus' to use
+	p3 = input PField variable
+
+	BGG, 11/2009
+*/
+
+#include <stdlib.h>
+#include <Instrument.h>
+#include <ugens.h>
+#include "PFSCHED.h"
+#include <rt.h>
+#include <rtdefs.h>
+
+#include <stdio.h>
+
+#include <PField.h>
+#include <PFieldSet.h>
+
+PFSCHED::PFSCHED() : Instrument()
+{
+}
+
+PFSCHED::~PFSCHED()
+{
+}
+
+int PFSCHED::init(double p[], int n_args)
+{
+// about ths RTBUFSAMPS+1 nonsense:
+// 	when a PFSCHED note finishes, it de-allocates the PFields associated
+// 	with it.  This is generally not a problem (because the 'connected'
+// 	instrument/note drawing from the PField only needs the PField for
+// 	the duration specified in the PFSCHED note), but if the duration is
+// 	less than one RTBUFSAMP then the PFSCHED note will finish and deallocate
+// 	*before* the other instrument gets a chance to read through the
+// 	'connected' PField.  Instead of rewriting a huge amount of the PField
+// 	code to handle the ref counters, etc. I just make sure that the
+// 	PFSCHED note will last at least one buffer longer than the time
+// 	computed for the PField to be read.  Almost no additional load
+// 	on the CPU, see the run() method below.
+
+	if (rtsetoutput(p[0], p[1]+((double)(RTBUFSAMPS+1)/SR), this) == -1)
+		return DONT_SCHEDULE;
+
+	pfbus = p[2];
+
+	pfbusses[pfbus].drawflag = 0; // the 'connected' note will read when == 1
+//	pfbusses[pfbus].thepfield = &((*_pfields)[3]);
+	pfbusses[pfbus].thepfield = &(getPField(3)); // this is the PField to read
+	pfbusses[pfbus].theincr = (SR/(float)resetval)/(double)(nSamps()-(RTBUFSAMPS+1));
+	// note the subtraction above, thus the PField will be read for the
+	// correct duration
+
+	return nSamps();
+}
+
+void PFSCHED::doupdate()
+{
+}
+
+int PFSCHED::run()
+{
+	int i;
+
+	pfbusses[pfbus].drawflag = 1; // signal to start reading from the pfield
+	i = framesToRun();
+	increment(i);
+
+	return i;
+}
+
+Instrument*
+makePFSCHED()
+{
+	PFSCHED *inst;
+	inst = new PFSCHED();
+	inst->set_bus_config("PFSCHED");
+	return inst;
+}
+
+void
+rtprofile()
+{
+	RT_INTRO("PFSCHED",makePFSCHED);
+}
+
---
+++ insts/bgg/PFSCHED/PFSCHED.h
@@ -0,0 +1,27 @@
+#include <PField.h>
+
+// each 'pfbus' is has the data in this structure.  It is declared
+// here and referenced in src/control/pfbus/PFBusPfield.cpp
+struct pfbusdata {
+	int drawflag;
+	const PField *thepfield;
+	double val;
+	double percent;
+	double theincr;
+};
+
+// this should be enough...
+#define NPFBUSSES 1024
+struct pfbusdata pfbusses[NPFBUSSES];
+
+class PFSCHED : public Instrument {
+	int pfbus;
+
+	void doupdate();
+
+public:
+	PFSCHED();
+	virtual ~PFSCHED();
+	virtual int init(double p[], int n_args);
+	virtual int run();
+};
