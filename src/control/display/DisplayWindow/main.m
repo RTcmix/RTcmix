800323160d12ed1f4fc088ca2bd85c0cb5e5d22a
jgibson <jgibson>
2011-08-07 15:43:20 +0000

    new xcode project

---
+++ src/control/display/DisplayWindow/main.m
@@ -0,0 +1,14 @@
+/* RTcmix - Copyright (C) 2011  The RTcmix Development Team
+   See ``AUTHORS'' for a list of contributors. See ``LICENSE'' for
+   the license to this software and for a DISCLAIMER OF ALL WARRANTIES.
+*/
+
+// -John Gibson, 8/3/11
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **) argv);
+}
+
