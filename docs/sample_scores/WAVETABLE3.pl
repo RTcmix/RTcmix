f64dd69a738e959669ae229e9ba8e8d23e9f8bcc
jgibson <jgibson>
2011-09-22 02:39:13 +0000

    Modernize and bring into line with current MinC version of same score.

--- docs/sample_scores/WAVETABLE3.pl
+++ docs/sample_scores/WAVETABLE3.pl
@@ -5,20 +5,24 @@ use RT;
 rtsetparams(44100, 2);
 load("WAVETABLE");
 print_off();
-makegen(1, 24, 1000, 0, 1,  950, 0);
-makegen(2, 10, 1000, 1, 0.3, 0.2);
+$maxamp = 4000;
+$amp = maketable("line", "nonorm", 5000, 0,0, .005,$maxamp, 1,0);
+$tabsize = 5000;
+$wavet = maketable("wave", $tabsize, 1, 0.3, 0.2);
+control_rate(44100);
 
-srand(0.35);
+srand(0);
 
 for ($start = 0; $start < 7; $start += 0.14) {
    $freq = (random() * 200) + 35;
    for ($i = 0; $i < 3; $i += 1) {
-      WAVETABLE($start, 0.4, 1500, $freq, 0);
-      WAVETABLE($start + random()*0.1, 0.4, 1500, $freq + (random()*7), 1);
+      WAVETABLE($start, 0.4, $amp, $freq, 0, $wavet);
+      WAVETABLE($start + random()*0.1, 0.4, $amp,
+				$freq + (random()*7), 1, $wavet);
       if ($start > 3.5) {
-         makegen(2, 10, 1000, 1, random(), random(), random(), random(),
-                  random(), random(), random(), random(), random(), random(),
-                  random());
+         $wavet = maketable("wave", $tabsize, 1, random(), random(), random(),
+							random(), random(), random(), random(), random(),
+							random(), random(), random());
       }
       $freq += 125;
    }
