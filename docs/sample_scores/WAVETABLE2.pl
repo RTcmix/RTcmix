693fffa721dadc803d9e779640af7236ebbdb26e
jgibson <jgibson>
2011-09-22 02:50:06 +0000

    Modernize and make consistent with WAVETABLE2.sco.

--- docs/sample_scores/WAVETABLE2.pl
+++ docs/sample_scores/WAVETABLE2.pl
@@ -1,25 +1,28 @@
-# This is a translation of sample_scos_1.0/WAVETABLE2.sco into Perl.   -JGG
+# This is a translation of sample_scores/WAVETABLE2.sco into Perl.   -JGG
 # Note the call to control_rate instead of reset.  We do that so as not
 # to conflict with Perl's reset function.
 
 use RT;
 
+print_off();	# gets rid of all the screen output
+
 rtsetparams(44100,2);
 load("WAVETABLE");
-# uncomment the following line to get rid of all the screen output
-#print_off();
-makegen(1, 24, 1000, 0,0, 0.01,1, 0.1,0.2, 0.4, 0);
-makegen(2, 10, 3000, 1, 0.3, 0.2);
+
+$amp = 10000;
+$env = maketable("line", 1000, 0,0, 0.01,1, 0.1,0.2, 0.4,0);
+$wavet = maketable("wave", 3000, 1, 0.3, 0.2);
 
 control_rate(20000);
 $start = 0.0;
 $freq = 149.0;
 
 for ($i = 0; $i < 100; $i++) {
-   WAVETABLE($start, 0.4, 5000, $freq, 0);
-   WAVETABLE($start + random()*0.07, 0.4, 5000, $freq + random()*2, 1);
+   WAVETABLE($start, 0.4, mul($env, $amp), $freq, 0, $wavet);
+   WAVETABLE($start + random()*0.07, 0.4, mul($env, $amp),
+		$freq + random()*2, 1, $wavet);
    $start += 0.1;
-   makegen(2, 10, 3000, 1, random(), random(), random(), random(),
+   $wavet = maketable("wave", 3000, 1, random(), random(), random(), random(),
                                                 random(), random());
 }
 
