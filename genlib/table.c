fe260f2dad5b3d88835ff6c4e4a92db0d92e148b
bgarton <bgarton>
2013-01-02 19:42:07 +0000

    added MAXMSP #ifdefs -- BGG

--- genlib/table.c
+++ genlib/table.c
@@ -1,4 +1,8 @@
+#ifdef MAXMSP
+float rtcmix_table(long nsample, double *array, float *tab)
+#else
 float table(long nsample, double *array, float *tab)
+#endif
 {
 	register int loc = ((float)(nsample)/(*tab)) * *(tab+1);
 	if(loc < 0) return(array[0]);
