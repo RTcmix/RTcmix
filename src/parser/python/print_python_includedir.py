a93ab9c0150b66986929be20713308fabe7ce4e0
John Gibson <johgibso@gmail.com>
2023-06-30 09:14:57 -0400

    Python 3 support.

--- src/parser/python/print_python_includedir.py
+++ src/parser/python/print_python_includedir.py
@@ -1,3 +1,8 @@
 import sysconfig
-print sysconfig.get_config_vars("INCLUDEPY")[0]
 
+#from distutils.sysconfig import get_python_inc
+#print(get_python_inc(plat_specific=1))
+
+#print(sysconfig.get_path('platinclude'))	# doesn't return valid path on my machine
+
+print(sysconfig.get_config_var("INCLUDEPY"))
