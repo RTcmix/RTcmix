131dfa86a86d7daff2e40e22eab897c98a844f6e
John Gibson <johgibso@gmail.com>
2023-06-30 09:12:42 -0400

    Python 3 support.

--- src/parser/python/make_python_ext.pl
+++ src/parser/python/make_python_ext.pl
@@ -1,6 +1,7 @@
 # This script writes the extension .c file to stdout.  It reads a list of
 # RTcmix function names from a file, and uses them in the .c file, so that
 # Python will recognize them in RTcmix user scripts.     -JGG, 12-Feb-01
+#                                          revised for python3, May 2022
 
 # names of module and verbatim src file passed in as args
 $module = $ARGV[0];
@@ -58,20 +59,40 @@ print "};\n\n";
 
 
 print qq~
-// Initialization function for the module (*must* be called init$module)
-DL_EXPORT(void) init$module()
+static struct PyModuleDef moduledef = {
+   PyModuleDef_HEAD_INIT,
+   .m_name = "rtcmix",
+	.m_doc = "Module that exposes RTcmix to Python",
+	.m_size = -1,
+	.m_methods = _rtcmix_methods,
+};
+
+// Initialization function for the module
+PyMODINIT_FUNC PyInit_$module()
 {
    // Initialize the type of the new type object here; doing it here
    // is required for portability to Windows without requiring C++.
-   OpaqueType.ob_type = &PyType_Type;
+   // FIXME: This no longer works, because ob_type isn't defined this way
+   // OpaqueType.ob_type = &PyType_Type;
+
+	if (PyType_Ready(&OpaqueType) < 0)
+		return NULL;
 
    // Create the module and add the functions
-   PyObject *m = Py_InitModule("$module", _rtcmix_methods); // aborts internally
+   PyObject *m = PyModule_Create(&moduledef);
+	if (m == NULL)
+		return NULL;
+
+   // AS: This builds, and is recommended by docs, but doesn't run some older stuff
+   Py_INCREF(&OpaqueType);
+   PyModule_AddObject(m, "rtcmix", (PyObject *) &OpaqueType);
 
    // Add some symbolic constants to the module
    PyObject *d = PyModule_GetDict(m);
    ErrorObject = PyErr_NewException((char *) "$module.error", NULL, NULL);
    PyDict_SetItemString(d, (char *) "error", ErrorObject);
+
+	return m;
 }
 
 } // extern "C"
