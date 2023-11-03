3c9cd86258f47e37b3ba7b925d58a03cd18644d8
Ian Clester <ijc@ijc8.me>
2023-11-03 09:50:41 -0400

    Update WebAssembly example

--- apps/wasmtest/glue.c
+++ apps/wasmtest/glue.c
@@ -25,36 +25,36 @@ float output[_FRAME_SIZE * _NUM_CHANNELS];
 int finished;
 
 void on_finish(long long frameCount, void *inContext) {
-	finished = 1;
+    finished = 1;
 }
 
 EMSCRIPTEN_KEEPALIVE
 void setup(int sample_rate) {
-	RTcmix_init();
-	RTcmix_setAudioBufferFormat(AudioFormat_32BitFloat_Normalized, NUM_CHANNELS);
-	RTcmix_setFinishedCallback(on_finish, NULL);
-	RTcmix_setparams(sample_rate, NUM_CHANNELS, FRAME_SIZE, 0, BUS_COUNT);
+    RTcmix_init();
+    RTcmix_setAudioBufferFormat(AudioFormat_32BitFloat_Normalized, NUM_CHANNELS);
+    RTcmix_setFinishedCallback(on_finish, NULL);
+    RTcmix_setparams(sample_rate, NUM_CHANNELS, FRAME_SIZE, 0, BUS_COUNT);
 }
 
 EMSCRIPTEN_KEEPALIVE
 void load_score(char *score) {
-	finished = 0;
+    finished = 0;
     RTcmix_parseScore(score, strlen(score));
 }
 
 EMSCRIPTEN_KEEPALIVE
 int process() {
-	if (finished) {
-		return 0;
-	}
-	RTcmix_runAudio(NULL, interleaved, FRAME_SIZE);
-	// Deinterleave channels for Web Audio API.
-	for (int frame = 0; frame < FRAME_SIZE; frame++) {
-		for (int channel = 0; channel < NUM_CHANNELS; channel++) {
-			output[channel * FRAME_SIZE + frame] = interleaved[frame * NUM_CHANNELS + channel];
-		}
-	}
-	return 1;
+    if (finished) {
+        return 0;
+    }
+    RTcmix_runAudio(NULL, interleaved, FRAME_SIZE);
+    // Deinterleave channels for Web Audio API.
+    for (int frame = 0; frame < FRAME_SIZE; frame++) {
+        for (int channel = 0; channel < NUM_CHANNELS; channel++) {
+            output[channel * FRAME_SIZE + frame] = interleaved[frame * NUM_CHANNELS + channel];
+        }
+    }
+    return 1;
 }
 
 EMSCRIPTEN_KEEPALIVE
