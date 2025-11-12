c38923ff1a3b7e0a4fa64c0bf50fb0079f0a1260
dougscott <dougscott@users.noreply.github.com>
2025-11-11 18:51:54 -0800

    Fixed bugs in OSX sending test.

--- test/suite/osc_send_test.cpp
+++ test/suite/osc_send_test.cpp
@@ -7,35 +7,54 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, char *argv[]){    
+void fail_error(const char *cmd)
+{
+	std::cout << "usage: " << cmd << " [-i <hostIP>] <scorefile>\nIf no hostIP, it will run locally.  If no scorefile is given, it will read from stdin." << std::endl;
+	exit(1);
+}
 
-    lo_address t = lo_address_new(NULL, "7777");
-	std::istream *input;
-    char *stuff = NULL;
-	if (argc == 1) {
-		input = &std::cin;
+int main(int argc, char *argv[]){    
+	const char *hostIP = NULL;
+	const char *filename = NULL;
+	std::ifstream ifs;            // keep it alive in this scope
+	std::istream *input = &std::cin;
+
+	if (argc == 1) {	// no args
 	}
-    else if (argc == 2) {
-        char *filename = argv[1];
-        std::ifstream ifs(filename);
+    else if (argc == 2) {	// filename
+		const char *arg = argv[1];
+		if (arg[0] != '-') {
+        	filename = arg;
+		}
+		else fail_error(argv[0]);
+	}
+	else if (argc > 2) {	// -i <IP>
+		const char *arg = argv[1];
+		if (arg[0] == '-' && arg[1] == 'i') {
+			hostIP = argv[2];
+		}
+		if (argc == 4) {
+			filename = argv[3];
+		}
+	}
+    else {
+		fail_error(argv[0]);
+    }
+	if (filename != NULL) {
+        ifs.open(filename);
         if (!ifs.is_open()) {
             fprintf(stderr, "Unable to open file '%s'\n", filename);
             return -1;
         }
 		input = &ifs;
 	}
-    else {
-        std::cout << "usage: " << argv[0] << " <scorefile>\nIf no scorefile is given, it will read from stdin." << std::endl;
-        return -1;
-    }
+    lo_address t = lo_address_new(hostIP, "7777");
 	std::string content( (std::istreambuf_iterator<char>(*input) ),
                    (std::istreambuf_iterator<char>()    ) );
-	stuff = new char[content.length()+1];
-	std::strcpy(stuff, content.c_str());
 
-    std::cout << "sending '" << stuff << "'" << std::endl;
+    std::cout << "sending '" << content << "'" << std::endl;
 
-    if (lo_send(t, "/RTcmix/ScoreCommands", "s", stuff) == -1) {
+    if (lo_send(t, "/RTcmix/ScoreCommands", "s", content.c_str()) == -1) {
         fprintf(stderr, "OSC error %d: %s\n", lo_address_errno(t),
                lo_address_errstr(t));
         return -1;
